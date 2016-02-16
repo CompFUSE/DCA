e773daab8716faaf18e0a78da8db34b144afa179
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-02-16 10:34:03 +0100

    solved a bug in test/c_s/CMakelist

--- build-aux/UNIX_clang.sh
+++ build-aux/UNIX_clang.sh
@@ -5,11 +5,10 @@
 ################################################################################
 
 # Mock EasyBuild.
-LOCAL_DIR=$(dirname $0)
 export EBROOTNFFT=/usr/local
 export EBROOTSPGLIB=/usr/local
-export EBROOTGTEST=$LOCAL_DIR/../libs/gmock-1.7.0/gtest/
+export EBROOTGTEST=/home/giovanni/Programs/DCA/libs/gmock-1.7.0/gtest/
 
 # Compilers
-export CXX="/usr/lib/mpic++"
-export CC=/usr/lib/mpic
+export CXX=/usr/lib/mpic++
+export CC=/usr/lib/mpicc
