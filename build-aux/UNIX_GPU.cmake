274226803493613a635c64034226dcf73a399eea
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-02-17 10:45:01 +0100

    modified application/dca/CMakeList to include libcusparse from CUDA_SPARSE_LIBRARY

--- build-aux/UNIX_GPU.cmake
+++ build-aux/UNIX_GPU.cmake
@@ -3,22 +3,16 @@
 # Usage: cmake -C path/to/UNIX_clang.cmake ..
 ################################################################################
 
-# MPIEXEC stuff for executing parallel tests.
-# Note: CXX and CC must be set as environment variables to the corresponding MPI
-#       C++ and C compiler wrappers.
+# call common option
+include(${CMAKE_CURRENT_SOURCE_DIR}/../build-aux/UNIX_clang.cmake)
 
-# Compile for Maxwell compute architecture.
 set(CUDA_GPU_ARCH "compute_50" CACHE STRING "gpu architecture" FORCE)
 # Change dafault cache entries.
 option(DCA_GPU_SUPPORT "Enable GPU support." ON)
 option(DCA_PTHREADS    "Enable pthreads"     ON)
-option( MAGMA_LIBRARY "Locate magma lib" "/usr/local/lib/libmagma.so")
+set(MAGMA_LIBRARY "/usr/local/lib/libmagma.a" CACHE FILEPATH "Location of magma lib" FORCE)
 set(CUDA_HOST_COMPILER "/usr/bin/gcc-4.9"
   CACHE STRING "Host side compiler used by NVCC")
-mark_as_advanced(CUDA_GPU_ARCH CUDA_HOST_COMPILER)
-
-set(MPIEXEC "/usr/bin/mpirun"
-  CACHE FILEPATH "Executable for running MPI programs.")
-set(MPIEXEC_NUMPROC_FLAG "-np"
-  CACHE FILEPATH
-  "Flag used by MPI to specify the number of processes for MPIEXEC; the next option will be the number of processes.")
+#set(CUDA_NVCC_FLAGS"${CUDA_NVCC_FLAGS}")
+set(CUDA_SPARSE_LIBRARY "/usr/local/cuda-7.5/targets/x86_64-linux/lib/libcusparse.so"  CACHE FILEPATH "libcusparse path" FORCE)
+mark_as_advanced(CUDA_GPU_ARCH CUDA_HOST_COMPILER) 
--- build-aux/UNIX_clang.cmake
+++ build-aux/UNIX_clang.cmake
@@ -5,12 +5,13 @@
 
 set(EBROOTNFFT "/usr/local" CACHE FILEPATH "NFFT location (no /lib)")
 set(EBROOTSPGLIB "/usr/local" CACHE FILEPATH "idem as before")
-set(EBROOTGTEST "${CMAKE_SOURCE_DIR}/libs/gmock-1.7.0/gtest/" CACHE FILEPATH "folder with gtest")
+set(EBROOTGTEST "${CMAKE_CURRENT_SOURCE_DIR}/../libs/gmock-1.7.0/gtest/" CACHE FILEPATH "folder with gtest")
 # Compilers for MPI
 set(MPI_CXX_COMPILER /usr/bin/mpic++)
 set(MPI_C_COMPILER /usr/bin/mpicc)
-
+#options and flafs
 option(DCA_GPU_SUPPORT "Disable GPU support." OFF)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS};-std=c++11;-fopenmp")
 
 
 # MPIEXEC stuff for executing parallel tests.
@@ -21,3 +22,4 @@ set(MPIEXEC "/usr/bin/mpirun"
 set(MPIEXEC_NUMPROC_FLAG "-np"
   CACHE FILEPATH
   "Flag used by MPI to specify the number of processes for MPIEXEC; the next option will be the number of processes.")
+mark_as_advanced(MPIEXEC_NUMPROG_FLAG MPIEXEC MPI_C_COMPILER MPI_CXX_COMPILER)
