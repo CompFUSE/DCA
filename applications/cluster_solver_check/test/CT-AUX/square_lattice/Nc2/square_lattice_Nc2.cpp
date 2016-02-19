61880c7ba9a92b2e6efabfcccfc65086387049e3
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-02-19 09:52:42 +0100

    Minor changes in the ct-aux tests.

--- applications/cluster_solver_check/test/CT-AUX/square_lattice/Nc2/square_lattice_Nc2.cpp
+++ applications/cluster_solver_check/test/CT-AUX/square_lattice/Nc2/square_lattice_Nc2.cpp
@@ -1,6 +1,8 @@
 //====================================================================
 // Copyright 2016 ETH Zurich.
 //
+// No-change test for CT-AUX and the square lattice.
+//
 // Author: Urs Haehner (haehneru@itp.phys.ethz.ch), ETH Zurich
 //====================================================================
 
@@ -24,19 +26,12 @@ TEST(DCA_loop_sp, Self_Energy) {
   using parameters_type = Parameters<dca_mpi_test_environment::concurrency_type,
                                      model, CT_AUX_CLUSTER_SOLVER>;
   using MOMS_type = DCA_data<parameters_type>;
-  // using ED_solver_type =
-  //     cluster_solver<ADVANCED_ED_CLUSTER_SOLVER, LIN_ALG::CPU,
-  //                    parameters_type, MOMS_type>;
   using quantum_cluster_solver_type =
-    cluster_solver<CT_AUX_CLUSTER_SOLVER, LIN_ALG::CPU, parameters_type, MOMS_type>;
+      cluster_solver<CT_AUX_CLUSTER_SOLVER, LIN_ALG::CPU, parameters_type,
+                     MOMS_type>;
   using QMC_solver_type = quantum_cluster_solver_type;
 
   if (dca_test_env->concurrency.id() == dca_test_env->concurrency.first()) {
-    std::cout << "\nDCA main starting.\n"
-              << "MPI-world set up: "
-              << dca_test_env->concurrency.number_of_processors()
-              << " processes.\n" << std::endl;
-
     GitVersion::print();
     Modules::print();
   }
@@ -51,7 +46,7 @@ TEST(DCA_loop_sp, Self_Energy) {
   MOMS_type MOMS_imag(parameters);
   MOMS_imag.initialize();
 
-  // Read ED data
+  // Read and broadcast ED data
   if (dca_test_env->concurrency.id() == dca_test_env->concurrency.first()) {
     IO::reader<IO::HDF5> reader;
     reader.open_file("data.ED.hdf5");
@@ -65,44 +60,43 @@ TEST(DCA_loop_sp, Self_Energy) {
   dca_test_env->concurrency.broadcast(MOMS_imag.Sigma);
   dca_test_env->concurrency.broadcast(MOMS_imag.G0_k_w_cluster_excluded);
   dca_test_env->concurrency.broadcast(MOMS_imag.G0_r_t_cluster_excluded);
-  
-  // QMC solver
-  // The QMC solver uses the free Greens function G0 computed by the ED solver.
-  // It is passed via the MOMS_imag object.
+
+  // Do one QMC iteration
   QMC_solver_type QMC_obj(parameters, MOMS_imag);
   QMC_obj.initialize(1);
   QMC_obj.integrate();
   QMC_obj.finalize(DCA_info_struct);
 
-  FUNC_LIB::function<std::complex<double>, dmn_4<nu,nu,k_DCA,w> >& Sigma_QMC = MOMS_imag.Sigma;
+  FUNC_LIB::function<std::complex<double>, dmn_4<nu, nu, k_DCA, w> >&
+      Sigma_QMC = MOMS_imag.Sigma;
 
+  // Read QMC self-energy from check_data file and compare it with the newly
+  // computed QMC self-energy.
   if (dca_test_env->concurrency.id() == dca_test_env->concurrency.first()) {
-    // Read self-energy from check_data file.
-    FUNC_LIB::function<std::complex<double>, dmn_4<nu,nu,k_DCA,w> > Sigma_QMC_check("Self_Energy");
+    FUNC_LIB::function<std::complex<double>, dmn_4<nu, nu, k_DCA, w> >
+        Sigma_QMC_check("Self_Energy");
     IO::reader<IO::HDF5> reader;
     reader.open_file("check_data.QMC.hdf5");
     reader.open_group("functions");
     reader.execute(Sigma_QMC_check);
     reader.close_file();
-    
-    // Compare the computed self-energy with the expected result.
+
     for (int w_ind = 0; w_ind < w::dmn_size(); ++w_ind) {
       for (int k_ind = 0; k_ind < k_DCA::dmn_size(); ++k_ind) {
         for (int nu_ind_2 = 0; nu_ind_2 < nu::dmn_size(); ++nu_ind_2) {
           for (int nu_ind_1 = 0; nu_ind_1 < nu::dmn_size(); ++nu_ind_1) {
-            EXPECT_NEAR(Sigma_QMC_check(nu_ind_1, nu_ind_2, k_ind, w_ind).real(),
-                        Sigma_QMC(nu_ind_1, nu_ind_2, k_ind, w_ind).real(), 1.e-14);
-            EXPECT_NEAR(Sigma_QMC_check(nu_ind_1, nu_ind_2, k_ind, w_ind).imag(),
-                        Sigma_QMC(nu_ind_1, nu_ind_2, k_ind, w_ind).imag(), 1.e-14);
+            EXPECT_NEAR(
+                Sigma_QMC_check(nu_ind_1, nu_ind_2, k_ind, w_ind).real(),
+                Sigma_QMC(nu_ind_1, nu_ind_2, k_ind, w_ind).real(), 1.e-14);
+            EXPECT_NEAR(
+                Sigma_QMC_check(nu_ind_1, nu_ind_2, k_ind, w_ind).imag(),
+                Sigma_QMC(nu_ind_1, nu_ind_2, k_ind, w_ind).imag(), 1.e-14);
           }
         }
       }
     }
   }
-  
-  if (dca_test_env->concurrency.id() == dca_test_env->concurrency.first()) {
-    std::cout << "\n\nCluster-solver-check ending.\n" << std::endl;
-  }
+
 }
 
 int main(int argc, char** argv) {
@@ -110,8 +104,8 @@ int main(int argc, char** argv) {
 
   ::testing::InitGoogleTest(&argc, argv);
 
-  dca_test_env = new dca_mpi_test_environment(
-      argc, argv, "input.square_lattice_Nc2.json");
+  dca_test_env =
+      new dca_mpi_test_environment(argc, argv, "input.square_lattice_Nc2.json");
   ::testing::AddGlobalTestEnvironment(dca_test_env);
 
   ::testing::TestEventListeners& listeners =
