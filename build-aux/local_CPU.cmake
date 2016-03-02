21df5bd7ba4ae3cff7997c8d61658b5e281979e5
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-03-02 09:23:49 +0100

    OLD folder renamed to unused_files Removed OLD folders in child directories

--- build-aux/local_CPU.cmake
+++ build-aux/local_CPU.cmake
@@ -1,21 +1,20 @@
+
 ################################################################################
 # Initial cache template for local machines
-# Usage: cmake -C path/to/this/file ..
+# Usage: 
+# Set CXX and CC to the desired compiler in the environment
+# cmake -C path/to/this/file ..
 ################################################################################
 
 ####################################################################
 #The user is expected to modify this block
 ####################################################################
 #libraries location.
-set(DCA_LIBDIR_NFFT   "" CACHE FILEPATH "NFFT directory") #don't include /lib
-set(DCA_LIBDIR_SPGLIB "" CACHE FILEPATH "SPGLIB directory") #don't include /lib
+set(DCA_LIBDIR_NFFT   "/path/to/nfft_root" CACHE FILEPATH "NFFT directory") #don't include /lib
+set(DCA_LIBDIR_SPGLIB "/path_to/spglib_root" CACHE FILEPATH "SPGLIB directory") #don't include /lib
 # Compilers for MPI
-set(MPIEXEC "" CACHE FILEPATH "Executable for running MPI programs.")
-set(CMAKE_CXX_COMPILER CACHE FILEPATH "")           #set to mpi wrapper compiler e.g. mpicxx
-set(CMAKE_C_COMPILER CACHE FILEPATH   "")           #set to mpi wrapper compiler e.g. mpicc
+set(MPIEXEC "/path/to/mpirun" CACHE FILEPATH "Executable for running MPI programs.")
 ####################################################################
-
-
 
 #google tests directory
 set(GTEST_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../libs/gmock-1.7.0/gtest/" CACHE FILEPATH "folder with gtest")
