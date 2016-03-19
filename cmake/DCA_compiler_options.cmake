0f4ac5f767d5d5eb79678e9fab5836b2690c4c29
John Biddiscombe <biddisco@cscs.ch>
2016-03-19 22:18:15 +0100

    Bump C++ flag to c++14

--- cmake/DCA_compiler_options.cmake
+++ cmake/DCA_compiler_options.cmake
@@ -8,6 +8,6 @@
 
 set(WARNINGS "")
 set(WARNINGS_RELEASE "-Wall -Wextra -Wpedantic -Werror -Wno-sign-compare -Wno-unused-parameter")
-set(FLAGS "-std=c++11")  # -funroll-loops -finline-functions
+set(FLAGS "-std=c++14")  # -funroll-loops -finline-functions
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNINGS} ${FLAGS}")
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${WARNINGS_RELEASE}")
