bcfb84d26962eae39f24e46609b82a84a97d36bf
John Biddiscombe <biddisco@cscs.ch>
2016-03-23 23:49:43 +0100

    Add metafunctions for typelist append/prepend/swap

--- src/comp_library/type_list/type_list.h
+++ src/comp_library/type_list/type_list.h
@@ -1,5 +1,6 @@
 //-*-C++-*-
 
+
 #ifndef TYPELIST_H_
 #define TYPELIST_H_
 
@@ -13,52 +14,58 @@ struct assert_same
     static_assert(std::is_same<T1, T2>::value, "Types must be equal");
 };
 //----------------------------------------------------------------------------
-class NullType {};
+class NullType {
+};
 
 //----------------------------------------------------------------------------
 // utility function : takes a variadic list or arguments and returns nothing.
 // It is used to convert a variadic pack expansion into a function so that
 // arbitrary functions can be called with a pack expansion and drop the results.
 //----------------------------------------------------------------------------
-template<typename...Ts>
-void ignore_returnvalues(Ts&&...) {}
+template<typename ...Ts>
+void ignore_returnvalues(Ts&&...) {
+}
 
 //----------------------------------------------------------------------------
 // From Simple C++11 metaprogramming, Peter Dimov
 // http://pdimov.com/cpp2/simple_cxx11_metaprogramming.html
 // interesting discussion of type transformations and examples
 // mp_xxxx metafunctions taken from this source
+//
+// See also "TypeLists and a TypeList Toolbox via Variadic Templates"
+// http://www.codeproject.com/Articles/1077852/TypeLists-and-a-TypeList-Toolbox-via-Variadic-Temp
 //----------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------
-// generic mp_list type list
-template<class... T>
-struct mp_list {};
+/// generic mp_list type list
+template<class ... T>
+struct mp_list {
+};
 
 /// create an alias for backward naming compatibility with DCA+ typelists
-template<class... Ts>
+template<class ... Ts>
 using Typelist = mp_list<Ts...>;
 
 //----------------------------------------------------------------------------
-// mp_rename A<...> -> B<...>
-template<class A, template<class...> class B>
+/// mp_rename A<...> -> B<...>
+template<class A, template<class ...> class B>
 struct mp_rename_impl;
 
-template<template<class...> class A, class... T, template<class...> class B>
+template<template<class ...> class A, class... T, template<class...> class B>
 struct mp_rename_impl<A<T...>, B>
 {
     using type = B<T...>;
 };
 
-template<class A, template<class...> class B>
+template<class A, template<class ...> class B>
 using mp_rename = typename mp_rename_impl<A, B>::type;
 
 //----------------------------------------------------------------------------
-// mp_size
+/// mp_size
 template<class L>
 struct mp_size_impl;
 
-template<template<class...> class L, class... T>
+template<template<class ...> class L, class... T>
 struct mp_size_impl<L<T...>>
 {
     using type = std::integral_constant<std::size_t, sizeof...(T)>;
@@ -68,28 +75,32 @@ template<class L>
 using mp_size = typename mp_size_impl<L>::type;
 
 //----------------------------------------------------------------------------
-// mp_plus
-template<class... T> struct mp_plus_impl;
+/// mp_plus
+template<class ... T>
+struct mp_plus_impl;
 
-template<class... T> using mp_plus = typename mp_plus_impl<T...>::type;
+template<class ... T>
+using mp_plus = typename mp_plus_impl<T...>::type;
 
-template<> struct mp_plus_impl<>
+template<>
+struct mp_plus_impl<>
 {
     using type = std::integral_constant<int, 0>;
 };
 
-template<class T1, class... T> struct mp_plus_impl<T1, T...>
+template<class T1, class ... T>
+struct mp_plus_impl<T1, T...>
 {
     static constexpr auto _v = T1::value + mp_plus<T...>::value;
 
     using type = std::integral_constant<
-        typename std::remove_const<decltype(_v)>::type, _v>;
+    typename std::remove_const<decltype(_v)>::type, _v>;
 };
 //----------------------------------------------------------------------------
-// mp_count
+/// mp_count
 template<class L, class V> struct mp_count_impl;
 
-template<template<class...> class L, class... T, class V>
+template<template<class ...> class L, class... T, class V>
 struct mp_count_impl<L<T...>, V>
 {
     using type = mp_plus<std::is_same<T, V>...>;
@@ -99,76 +110,135 @@ template<class L, class V>
 using mp_count = typename mp_count_impl<L, V>::type;
 
 //----------------------------------------------------------------------------
+/// mp_append
+template<typename T, typename ...TL>
+struct mp_append;
+
+template<typename T, typename ...Ts>
+struct mp_append<mp_list<Ts...>, T>
+{
+    typedef mp_list<Ts..., T> type;
+};
+
+template<typename ...Ts1, typename ...Ts2>
+struct mp_append<mp_list<Ts1...>, mp_list<Ts2...>>
+{
+    typedef mp_list<Ts1..., Ts2...> type;
+};
+
+//----------------------------------------------------------------------------
+/// mp_prepend
+template<typename T, typename ...TL>
+struct mp_prepend;
+
+template<typename T, typename ...Ts>
+struct mp_prepend<mp_list<Ts...>, T>
+{
+    typedef mp_list<T, Ts...> type;
+};
+
+template<typename ...Ts1, typename ...Ts2>
+struct mp_prepend<mp_list<Ts1...>, mp_list<Ts2...>>
+{
+    typedef mp_list<Ts2..., Ts1...> type;
+};
+
+//----------------------------------------------------------------------------
 // From : True Story: Efficient Packing
 // http://talesofcpp.fusionfenix.com/post-22/true-story-efficient-packing
 // get Nth type from typelist/tuple efficiently without recursion
 //----------------------------------------------------------------------------
-template <std::size_t I, typename T>
+template<std::size_t I, typename T>
 struct _indexed {
-  using type = T;
+    using type = T;
 };
 
-template <typename Is, typename ...Ts>
+template<typename Is, typename ...Ts>
 struct _indexer;
 
-template <std::size_t ...Is, typename ...Ts>
-struct _indexer<std::index_sequence<Is...>, Ts...> : _indexed<Is, Ts>...
-{};
+template<std::size_t ...Is, typename ...Ts>
+struct _indexer<std::index_sequence<Is...>, Ts...> : _indexed<Is, Ts> ...
+{
+};
 
-template <std::size_t I, typename ...Ts>
+template<std::size_t I, typename ...Ts>
 struct _at_index {
-  template <typename T>
-  static _indexed<I, T> _select(_indexed<I, T>);
+    template<typename T>
+    static _indexed<I, T> _select(_indexed<I, T>);
 
-  using _impl = _indexer<std::index_sequence_for<Ts...>, Ts...>;
-  using type = typename decltype(_select(_impl{}))::type;
+    using _impl = _indexer<std::index_sequence_for<Ts...>, Ts...>;
+    using type = typename decltype(_select(_impl {}))::type;
 };
 
 // was tuple_element, but we want typelist_element
-template <std::size_t I, typename Tuple>
+template<std::size_t I, typename Tuple>
 struct mp_element;
 
-template <std::size_t I, typename ...Ts>
+template<std::size_t I, typename ...Ts>
 struct mp_element<I, Typelist<Ts...>> : _at_index<I, Ts...>
-{};
+{
+};
 
 //-----------------------------------------------------------------------------
-// Search a typelist for a first occurrence of the type T
+/// Search a typelist for a first occurrence of the type T
 
 // Implementation: has index as a template parameter
-    template<size_t idx, typename T, class List>
-    struct mp_index_of_impl;
+template<size_t idx, typename T, class List>
+struct mp_index_of_impl;
 
-    template<size_t idx, typename T> /// The type T is not in the list
-    struct mp_index_of_impl<idx, T, mp_list<>>
-    {
-        using type = std::integral_constant<int, -1>;
-    };
+template<size_t idx, typename T> /// The type T is not in the list
+struct mp_index_of_impl<idx, T, mp_list<>>
+{
+    using type = std::integral_constant<int, -1>;
+};
 
-    template<size_t idx, typename T, typename ... Ts>    ///> The type is found
-    struct mp_index_of_impl<idx, T, mp_list<T, Ts...>>
-    {
-        using type = std::integral_constant<int, idx>;
-    };
+template<size_t idx, typename T, typename ... Ts>    ///> The type is found
+struct mp_index_of_impl<idx, T, mp_list<T, Ts...>>
+{
+    using type = std::integral_constant<int, idx>;
+};
 
-    template<size_t idx, typename T, typename H, typename ... Ts>  ///> Recursion
-    struct mp_index_of_impl<idx, T, mp_list<H, Ts...>>
-    {
-        using type = typename mp_index_of_impl<idx + 1, T, mp_list<Ts...>>::type;
-    };
+template<size_t idx, typename T, typename H, typename ... Ts>  ///> Recursion
+struct mp_index_of_impl<idx, T, mp_list<H, Ts...>>
+{
+    using type = typename mp_index_of_impl<idx + 1, T, mp_list<Ts...>>::type;
+};
 
-    // Wrapping to supply initial index 0
-    template<typename T, class List>
-    struct mp_index_of;
+// Wrapping to supply initial index 0
+template<typename T, class List>
+struct mp_index_of {
+    static constexpr int value = -1;
+};
 
-    // Specializing for idx >= 0
-    template<typename T, typename ... Ts>
-    struct mp_index_of<T, mp_list<Ts...>>
-    {
-        using type = typename mp_index_of_impl<0, T, mp_list<Ts...>>::type;
-        using value_type = typename type::value_type;
-        static constexpr value_type value = type::value;
-    };
+// Specializing for idx >= 0
+template<typename T, typename ... Ts>
+struct mp_index_of<T, mp_list<Ts...>>
+{
+    using type = typename mp_index_of_impl<0, T, mp_list<Ts...>>::type;
+    using value_type = typename type::value_type;
+    static constexpr value_type value = type::value;
+};
+
+//-----------------------------------------------------------------------------
+/// swap first element out of a typelist
+template<typename TList, typename T1, typename T2>
+struct mp_swap {
+};
+
+template<typename T1, typename T2>
+struct mp_swap<mp_list<>, T1, T2> {
+    typedef mp_list<> type;
+};
+
+template<typename T1, typename T2, typename ...Ts>
+struct mp_swap<mp_list<T1, Ts...>, T1, T2> {
+    typedef mp_list<T2, Ts...> type;
+};
+
+template<typename T0, typename ...Ts, typename T1, typename T2>
+struct mp_swap<mp_list<T0, Ts...>, T1, T2> {
+    typedef Typelist<T0, typename mp_swap<mp_list<Ts...>, T1, T2>::type> type;
+};
 
 //----------------------------------------------------------------------------
 // namespace TL
@@ -193,115 +263,38 @@ namespace TL
     /***********************************
      * TypeAt
      ***********************************/
-    template <int I, typename ...Ts>
+    template<int I, typename ...Ts>
     using TypeAt = mp_element<I, Ts...>;
 
     /***********************************
      * 	IndexOf
      ***********************************/
-    template <typename T, typename ...Ts>
+    template<typename T, typename ...Ts>
     using IndexOf = mp_index_of<T,Ts...>;
-
-    /***********************************
-     * 	IndexOf_At
-     ***********************************/
-/*
-    template <class TList, class T, int N>
-    struct IndexOf_At;
-
-    template <class T, int N>
-    struct IndexOf_At<NullType, T, N>
-    {
-        enum { value = -1 };
-    };
-
-    template <class Tail, class T>
-    struct IndexOf_At<Typelist<T, Tail>, T, 0>
-    {
-    private:
-        enum { temp = 0 };
-    public:
-        enum { value = temp == -1 ? -1 : 1 + temp };
-    };
-
-    template <class Tail, class T, int N>
-    struct IndexOf_At<Typelist<T, Tail>, T, N>
-    {
-    private:
-        enum { temp = IndexOf_At<Tail, T, N-1>::value };
-    public:
-        enum { value = temp == -1 ? -1 : 1 + temp };
-    };
-
-    template <class Head, class Tail, class T, int N>
-    struct IndexOf_At<Typelist<Head, Tail>, T, N>
-    {
-    private:
-        enum { temp = IndexOf_At<Tail, T, N>::value };
-    public:
-        enum { value = temp == -1 ? -1 : 1 + temp };
-    };
-
-*/
 
     /**********************************
      * 	Append
      **********************************/
-/*
-    template <class TList, class T> struct Append;
+    template<typename T1, typename T2>
+    using Append = mp_append<T1,T2>;
 
-    template <>
-    struct Append<NullType, NullType>
-    {
-        typedef NullType Result;
-    };
+    /**********************************
+     * Prepend
+     **********************************/
+    template<typename T1, typename T2>
+    using Prepend = mp_prepend<T1,T2>;
 
-    template <class T>
-    struct Append<NullType, T>
-    {
-        typedef TYPELIST_1(T) Result;
-    };
-
-    template <class Head, class Tail>
-    struct Append<NullType, Typelist<Head, Tail> >
-    {
-        typedef Typelist<Head, Tail> Result;
-    };
-
-    template <class Head, class Tail, class T>
-    struct Append<Typelist<Head, Tail>, T>
-    {
-        typedef Typelist<Head, typename Append<Tail, T>::Result> Result;
-    };
-*/
     /***********************************
      * 	Swap
      ***********************************/
-/*
-    template <class TList,class T1,class T2>
-    struct Swap;
-
-    template <class T1,class T2>
-    struct Swap<NullType, T1, T2> {
-        typedef NullType Result;
-    };
-
-    template <class T1, class T2,class Tail>
-    struct Swap<Typelist<T1,Tail>,T1, T2> {
-        typedef Typelist<T2,Tail> Result;
-    };
-
-    template <class Head, class Tail, class T1, class T2>
-    struct Swap<Typelist<Head,Tail>,T1,T2> {
-        typedef Typelist<Head, typename Swap<Tail,T1,T2>::Result> Result;
-    };
-*/
+    template<typename T1, typename T2, typename T3>
+    using Swap = mp_swap<T1,T2,T3>;
 
     /***********************************
      * 	Print
      ***********************************/
-/*
-    template <typename Head>
+
+    template<typename Head>
     struct print_typename {
         static void print() {
             //std::cout << "\t" << __PRETTY_FUNCTION__ << std::endl;
@@ -309,15 +302,10 @@ namespace TL
         }
     };
 
-    template <class Head>
+    template<class Head>
     struct print_type {
         static void print() {
             std::cout << "\t" << __PRETTY_FUNCTION__ << std::endl;
-            std::cout << "\t" << Head::get_name() << std::endl;
-        }
-
-        static void print(std::ostream &s) {
-            s << "\t" << __PRETTY_FUNCTION__ << std::endl;
             //std::cout << "\t" << Head::get_name() << std::endl;
         }
 
@@ -332,32 +320,32 @@ namespace TL
             //ss << "\t" << Head::get_name() << std::endl;
         }
     };
-
-    template <class Ts>
+/*
+    template<class TL>
     struct printTL {
-        static void print(std::ostream &s) {
-//            print_type<typename TL::TypeAt<0,Ts>::type>::print(s);
-//            printTL<typename TL::Tail>::print(s);
+        static void print() {
+            print_type<typename TL::Head>::print();
+            printTL<typename TL::Tail>::print();
         }
 
         static void print(std::stringstream& ss) {
-//            print_type<typename TL::Head>::print(ss);
-//            printTL<typename TL::Tail>::print(ss);
+            print_type<typename TL::Head>::print(ss);
+            printTL<typename TL::Tail>::print(ss);
         }
 
         template<class stream_type>
         static void to_JSON(stream_type& ss) {
             ss << "\"";
-//            print_type<typename TL::Head>::to_JSON(ss);
+            print_type<typename TL::Head>::to_JSON(ss);
 
-//            if(Length<typename TL::Tail>::value != 0)
-//                ss << "\",\n";
+            if (Length<typename TL::Tail>::value != 0)
+                ss << "\",\n";
 
-//            printTL<typename TL::Tail>::to_JSON(ss);
+            printTL<typename TL::Tail>::to_JSON(ss);
         }
     };
 
-    template <>
+    template<>
     struct printTL<NullType> {
         static void print() {
             std::cout << "[end]" << std::endl;
@@ -375,6 +363,5 @@ namespace TL
 */
 
 }
-
 
 #endif
