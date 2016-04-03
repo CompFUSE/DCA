28178fb58ae11d92c722f062c7f7e95718093f5d
John Biddiscombe <biddisco@cscs.ch>
2016-04-03 12:15:56 +0200

    Check CXX features (compiler compatibility) using a new CXX feature check macro

---
+++ cmake/DCA_cmake_features.cmake
@@ -0,0 +1,57 @@
+#------------------------------------------------------------------------------
+# (pretty) Print a list with a title and the list length
+#------------------------------------------------------------------------------
+function(print_list title list_in)
+  list(LENGTH list_in LIST_LENGTH)
+  message("================ ${title} (${LIST_LENGTH}) ================ ")
+  foreach (_item ${list_in})
+    message("${_item}")
+  endforeach()
+  message("================")
+endfunction()
+
+#------------------------------------------------------------------------------
+# Remove all items that appear in list1 from list2
+#------------------------------------------------------------------------------
+function(remove_from_list list1 list2 out_list)
+  foreach(_item ${list1})
+    list(FIND list2 ${_item} _pos)
+    if(NOT _pos STREQUAL "-1")
+      list(REMOVE_ITEM list2 ${_item})
+    endif()
+  endforeach()
+  set(${out_list} "${list2}" PARENT_SCOPE)
+endfunction()
+
+#------------------------------------------------------------------------------
+# Check if all user supplied features are supported
+# In : list of variable length num params
+#------------------------------------------------------------------------------
+function(DCA_check_cxx_features)
+  set(_feature_list "${ARGN}")
+  #print_list("Requested features" "${_feature_list}")
+
+  # Get all CXX features known to CMake
+  get_property(cxx_known_features GLOBAL PROPERTY CMAKE_CXX_KNOWN_FEATURES)
+  # list(SORT cxx_known_features)
+
+  # Get all CXX supported by the compiler
+  set(cxx_compile_features ${CMAKE_CXX_COMPILE_FEATURES})
+  # list(SORT cxx_compile_features)
+
+  # (debug) Remove supported features from known features list
+  remove_from_list("${cxx_compile_features}" "${cxx_known_features}" unsupported_features)
+
+  # Remove supported features from requested feature list
+  remove_from_list("${cxx_compile_features}" "${_feature_list}" unsupported_requested)
+  list(LENGTH unsupported_requested LENGTH_UNSUPPORTED)
+  if(LENGTH_UNSUPPORTED GREATER 0)
+    print_list("Requested unsupported CXX features" "${unsupported_requested}")
+    print_list("All unsupported CXX features" "${unsupported_features}")
+    message(WARNING "This project requires unsupported CXX features\n" 
+      "@@@@@@@@@@@@@@@@@@@@@\n"
+      "Get a better compiler\n"
+      "@@@@@@@@@@@@@@@@@@@@@\n"
+    )
+  endif()
+endfunction()
