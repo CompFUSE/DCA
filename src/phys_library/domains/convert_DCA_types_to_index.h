c04ddf41af1f153444157365fc63e8567f9d1db9
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-06-07 13:56:13 +0200

    Cleanup of of rest of domains: Make each header file self-contained and conform with coding-style, remove file that includes everything.

--- src/phys_library/domains/convert_DCA_types_to_index.h
+++ src/phys_library/domains/convert_DCA_types_to_index.h
@@ -1,78 +1,80 @@
-//-*-C++-*-
-  /*!
-   *  \author: Peter Staar
-   */
-#ifndef CONVERT_DCA_TYPES_TO_INDEX_H
-#define CONVERT_DCA_TYPES_TO_INDEX_H
-#include "phys_library/domain_types.hpp"
-using namespace types;
+// Copyright (C) 2009-2016 ETH Zurich
+// Copyright (C) 2007?-2016 Center for Nanophase Materials Sciences, ORNL
+// All rights reserved.
+//
+// See LICENSE.txt for terms of usage.
+// See CITATION.txt for citation guidelines if you use this code for scientific publications.
+//
+// Author: Peter Staar (peter.w.j.staar@gmail.com)
+//
+// This class implements various types and orderings of the matsubara-frequency domain via
+// templates.
 
-namespace QMC
-{
+#ifndef PHYS_LIBRARY_DOMAINS_CONVERT_DCA_TYPES_TO_INDEX_H
+#define PHYS_LIBRARY_DOMAINS_CONVERT_DCA_TYPES_TO_INDEX_H
 
-  template<typename target, class whatever>
-  class convert{};
-  template<>
-  class convert<int, dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> > >
-  {
-  public:
+#include "comp_library/function_library/include_function_library.h"
+#include "phys_library/domains/Quantum_domain/electron_band_domain.h"
+#include "phys_library/domains/Quantum_domain/electron_spin_domain.h"
 
-    static int spin_orbital(int band, e_spin_states_type e_spin);
+namespace QMC {
+template <typename target, class whatever>
+class convert {};
+template <>
+class convert<int, dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>> {
+public:
+  using b = dmn_0<electron_band_domain>;
+  using s = dmn_0<electron_spin_domain>;
+  using nu = dmn_variadic<b, s>;  // orbital-spin domain
 
-  private:
-    static FUNC_LIB::function<int, nu>& intitialize_spin_orbital();
-  };
+  static int spin_orbital(int band, e_spin_states_type e_spin);
 
-  int convert<int, dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> > >::spin_orbital(int band,
-                                                                                                   e_spin_states_type e_spin)
-  {
-    typedef nu::this_type parameter_typelist;
+private:
+  static FUNC_LIB::function<int, nu>& intitialize_spin_orbital();
+};
 
-    static FUNC_LIB::function<int, nu>& spo_function = intitialize_spin_orbital();
+int convert<int, dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>>::spin_orbital(
+    int band, e_spin_states_type e_spin) {
+  typedef nu::this_type parameter_typelist;
 
-    if(std::is_same<TypeAt<0,parameter_typelist>::type, electron_band_domain>::value)
-      {
-        return spo_function(band, electron_spin_domain::to_coordinate(e_spin));
-        //return spo_function(band, do_cast<int>::execute(e_spin));
-      }
-    else
-      {
-        return spo_function(electron_spin_domain::to_coordinate(e_spin), band);
-        //return spo_function(do_cast<int>::execute(e_spin), band);
-      }
+  static FUNC_LIB::function<int, nu>& spo_function = intitialize_spin_orbital();
+
+  if (std::is_same<dca::util::TypeAt<0, parameter_typelist>::type, electron_band_domain>::value) {
+    return spo_function(band, electron_spin_domain::to_coordinate(e_spin));
+    // return spo_function(band, do_cast<int>::execute(e_spin));
   }
-
-  FUNC_LIB::function<int, dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> > >&
-  convert<int, dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> > >::intitialize_spin_orbital()
-  {
-    static FUNC_LIB::function<int, nu> spo_function;
-
-    for(int i=0; i<spo_function.size(); i++)
-      spo_function(i) = i;
-
-    return spo_function;
+  else {
+    return spo_function(electron_spin_domain::to_coordinate(e_spin), band);
+    // return spo_function(do_cast<int>::execute(e_spin), band);
   }
-
-  /*!
-   *  \author: Peter Staar
-   */
-  template<>
-  class convert<dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> >, int>
-  {
-
-  public:
-
-    static std::pair<int,int> from_spin_orbital(int spo);
-  };
-
-  std::pair<int,int> convert<dmn_2<dmn_0<electron_band_domain>,dmn_0<electron_spin_domain> >, int>::from_spin_orbital(int spo)
-  {
-    std::pair<int,int> tmp;
-    tmp.first  = spo % b::dmn_size();
-    tmp.second = (spo-tmp.first) % s::dmn_size();
-    return tmp;
-  }
-
 }
 
-#endif
+FUNC_LIB::function<int, dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>>& convert<
+    int, dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>>::intitialize_spin_orbital() {
+  static FUNC_LIB::function<int, nu> spo_function;
+
+  for (int i = 0; i < spo_function.size(); i++)
+    spo_function(i) = i;
+
+  return spo_function;
+}
+
+template <>
+class convert<dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>, int> {
+public:
+  using b = dmn_0<electron_band_domain>;
+  using s = dmn_0<electron_spin_domain>;
+
+  static std::pair<int, int> from_spin_orbital(int spo);
+};
+
+std::pair<int, int> convert<dmn_2<dmn_0<electron_band_domain>, dmn_0<electron_spin_domain>>,
+                            int>::from_spin_orbital(int spo) {
+  std::pair<int, int> tmp;
+  tmp.first = spo % b::dmn_size();
+  tmp.second = (spo - tmp.first) % s::dmn_size();
+  return tmp;
+}
+}
+
+#endif  // PHYS_LIBRARY_DOMAINS_CONVERT_DCA_TYPES_TO_INDEX_H
