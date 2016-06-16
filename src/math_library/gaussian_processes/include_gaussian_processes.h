420cb264c75009f4cfcc037cc97b359bf995c25f
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-06-16 15:37:41 +0200

    Remove some collected include files.

--- src/math_library/gaussian_processes/include_gaussian_processes.h
+++
@@ -1,9 +0,0 @@
-//-*-C++-*-
-
-#include "covariance_function/covariance_function.h"
-#include "covariance_function/covariance_function_square_exponential.h"
-#include "covariance_function/covariance_function_periodic_square_exponential.h"
-
-#include "gaussian_fit.h"
-
-
