a2bf800a944f6046a8d734ce82d5c6d57dca82ec
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-08-19 15:53:14 +0200

    Change signature of range initialization and add preconditions to indices initialization.

--- src/comp_library/function_library/domains/special_domains/reduced_domain.hpp
+++ src/comp_library/function_library/domains/special_domains/reduced_domain.hpp
@@ -9,16 +9,16 @@
 //         Urs R. Haehner (haehneru@itp.phys.ethz.ch)
 //
 // This file implements a class to store a subset of a previously defined one-dimensional domain.
-// This subset can either be a set of single elements or a range starting from either the beginning
-// or the middle index (more precisely base-domain-size/2) of the base domain. The domain is created
-// by a type definition followed by a call to an initialize function.
+// This subset can either be a set of single elements or a range of the original domain.
+// The domain is created by a type definition followed by a call to an initialize function.
 //
 // INTERNAL: This class is probably good enough for its purpose, but it doesn't seem to be
-// 'complete'.
+//           'complete'.
 
 #ifndef COMP_LIBRARY_FUNCTION_LIBRARY_DOMAINS_SPECIAL_DOMAINS_REDUCED_DOMAIN_HPP
 #define COMP_LIBRARY_FUNCTION_LIBRARY_DOMAINS_SPECIAL_DOMAINS_REDUCED_DOMAIN_HPP
 
+#include <algorithm>  // for std::is_sorted
 #include <cassert>
 #include <cstdlib>  // for std::size_t
 #include <stdexcept>
@@ -35,14 +35,13 @@ class ReducedDomain {
 public:
   using ElementType = typename BaseDomain::element_type;
 
-  // If from_middle = true, creates a domain that contains the first n elements of the base domain
-  // counted from the middle index (= base-domain-size/2) of the base domain.
-  // If from_middle = false, creates a domain that contains the first n elements of the base domain.
-  static void initialize(const std::size_t n, const bool from_middle, const std::string& name = "");
+  // Creates a domain that contains the elements in the range [first, last) of the base domain.
+  static void initialize(const std::size_t first, const std::size_t last,
+                         const std::string& name = "");
 
   // Creates a domain that only contains the elements of the base domain with the given indices.
-  // INTERNAL: What should be the preconditions on indicies? Sorted, no duplicates,
-  //           size <= base-domain-size?
+  // Preconditions: - 0 <= indices[i] < base-domain-size, for 0 <= i < indices.size().
+  //                - indices is sorted and doesn't contain duplicates.
   static void initialize(const std::vector<std::size_t>& indices, const std::string& name = "");
 
   // Use '_' (underscore) to be consistent with other domain classes.
@@ -75,23 +74,21 @@ template <class BaseDomain>
 std::vector<typename ReducedDomain<BaseDomain>::ElementType> ReducedDomain<BaseDomain>::elements_;
 
 template <class BaseDomain>
-void ReducedDomain<BaseDomain>::initialize(const std::size_t n, const bool from_middle,
+void ReducedDomain<BaseDomain>::initialize(const std::size_t first, const std::size_t last,
                                            const std::string& name) {
   commonInit(name);
 
   const std::size_t base_size = BaseDomain::dmn_size();
 
-  if (from_middle) {
-    if (n > (base_size + 1) / 2)
-      throw std::out_of_range("Range to copy goes beyond end of base domain.");
-  }
-  else {
-    if (n > base_size)
-      throw std::out_of_range("Range to copy goes beyond end of base domain.");
-  }
+  if (first >= base_size)
+    throw std::out_of_range("First index must be < base-domain-size.");
+  if (last > base_size)
+    throw std::out_of_range("Last index must be <= base-domain-size.");
+  if (first > last)
+    throw std::logic_error("Last index must be >= first index.");
 
-  elements_.assign(BaseDomain::get_elements().begin() + from_middle * base_size / 2,
-                   BaseDomain::get_elements().begin() + from_middle * base_size / 2 + n);
+  elements_.assign(BaseDomain::get_elements().begin() + first,
+                   BaseDomain::get_elements().begin() + last);
 }
 
 template <class BaseDomain>
@@ -99,14 +96,17 @@ void ReducedDomain<BaseDomain>::initialize(const std::vector<std::size_t>& indic
                                            const std::string& name) {
   commonInit(name);
 
-  if (indices.size() > BaseDomain::dmn_size())
-    throw std::out_of_range("More indices than elements in the base domain.");
+  if (!std::is_sorted(indices.begin(), indices.end()))
+    throw std::logic_error("Indices must be sorted.");
 
   elements_.resize(indices.size());
 
   for (std::size_t i = 0; i < indices.size(); i++) {
     if (indices[i] >= BaseDomain::dmn_size())
       throw std::out_of_range("Indices must be < base-domain-size.");
+    if (i > 0 && indices[i - 1] == indices[i])
+      throw std::logic_error("Vector with indices contains duplicates.");
+
     elements_[i] = BaseDomain::get_elements()[indices[i]];
   }
 }
@@ -114,7 +114,7 @@ void ReducedDomain<BaseDomain>::initialize(const std::vector<std::size_t>& indic
 template <class BaseDomain>
 void ReducedDomain<BaseDomain>::commonInit(const std::string& name) {
   if (initialized_)
-    throw(std::logic_error("Domain has already been initialized."));
+    throw std::logic_error("Domain has already been initialized.");
 
   initialized_ = true;
 
--- test/unit/function/domains/reduced_domain_test.cpp
+++ test/unit/function/domains/reduced_domain_test.cpp
@@ -66,112 +66,105 @@ TEST(ReducedDomainTest, InitializeWithIndices) {
 }
 
 TEST(ReducedDomainTest, InitializeWithIndicesExceptions) {
-  // More indicies than elements in the base domain
+  // Index >= base-domain-size
   using BaseDmn1 = dmn<2, double>;
   BaseDmn1::set_elements(std::vector<double>{0., 10.});
 
   using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
-  EXPECT_THROW(ReducedDmn1::initialize(std::vector<std::size_t>{0, 1, 2}), std::out_of_range);
+  EXPECT_THROW(ReducedDmn1::initialize(std::vector<std::size_t>{0, 2}), std::out_of_range);
 
-  // Index >= base-domain-size
+  // Vector of indices isn't sorted.
   using BaseDmn2 = dmn<3, double>;
   BaseDmn2::set_elements(std::vector<double>{0., 10., 20.});
 
   using ReducedDmn2 = dca::func::ReducedDomain<BaseDmn2>;
-  EXPECT_THROW(ReducedDmn2::initialize(std::vector<std::size_t>{1, 3}), std::out_of_range);
+  EXPECT_THROW(ReducedDmn2::initialize(std::vector<std::size_t>{2, 0}), std::logic_error);
+
+  // Vector of indices contains duplicates.
+  using BaseDmn3 = dmn<4, double>;
+  BaseDmn3::set_elements(std::vector<double>{0., 10., 20., 40.});
+
+  using ReducedDmn3 = dca::func::ReducedDomain<BaseDmn3>;
+  EXPECT_THROW(ReducedDmn3::initialize(std::vector<std::size_t>{1, 1, 2}), std::logic_error);
 }
 
 TEST(ReducedDomainTest, ReinitializationException) {
-  using BaseDmn1 = dmn<4, double>;
-  BaseDmn1::set_elements(std::vector<double>{0., 10., 20., 30.});
+  using BaseDmn1 = dmn<5, double>;
+  BaseDmn1::set_elements(std::vector<double>{0., 10., 20., 30., 40.});
 
   using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
   ReducedDmn1::initialize(std::vector<std::size_t>{0, 1, 2});
   EXPECT_THROW(ReducedDmn1::initialize(std::vector<std::size_t>{0, 1}), std::logic_error);
-  EXPECT_THROW(ReducedDmn1::initialize(2, false), std::logic_error);
+  EXPECT_THROW(ReducedDmn1::initialize(2, 4), std::logic_error);
 }
 
-TEST(ReducedDomainTest, InitializeWithRangeFromFirst) {
-  using BaseDmn1 = dmn<4, long>;
-  BaseDmn1::set_elements(std::vector<long>{0, 10, 20, 30});
+TEST(ReducedDomainTest, InitializeWithRange) {
+  using BaseDmn1 = dmn<5, long>;
+  BaseDmn1::set_elements(std::vector<long>{0, 10, 20, 30, 40});
 
   using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
-  ReducedDmn1::initialize(3, false, "ReducedDomain-intialized-with-range");
+  ReducedDmn1::initialize(1, 4, "ReducedDomain-intialized-with-range");
 
-  const std::vector<long> check1{0, 10, 20};
+  const std::vector<long> check1{10, 20, 30};
   EXPECT_EQ(3, ReducedDmn1::get_size());
   EXPECT_EQ(check1, ReducedDmn1::get_elements());
   EXPECT_EQ("ReducedDomain-intialized-with-range", ReducedDmn1::get_name());
 
-  // Zero
-  using BaseDmn2 = dmn<5, long>;
-  BaseDmn2::set_elements(std::vector<long>{0, 10, 20, 30, 40});
+  // first = 0
+  using BaseDmn2 = dmn<6, long>;
+  BaseDmn2::set_elements(std::vector<long>{0, 10, 20, 30, 40, 50});
 
   using ReducedDmn2 = dca::func::ReducedDomain<BaseDmn2>;
-  ReducedDmn2::initialize(0, false);
+  ReducedDmn2::initialize(0, 4);
 
-  EXPECT_EQ(0, ReducedDmn2::get_size());
-  EXPECT_EQ(std::vector<long>{}, ReducedDmn2::get_elements());
-  EXPECT_EQ("dmn<5, long>_reduced", ReducedDmn2::get_name());
-}
-
-TEST(ReducedDomainTest, InitializeWithRangeFromFirstExeceptions) {
-  // Range to copy goes beyond end of base domain
-  using BaseDmn1 = dmn<4, float>;
-  BaseDmn1::set_elements(std::vector<float>{-1., -2., -3., -4.});
-
-  using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
-  EXPECT_THROW(ReducedDmn1::initialize(5, false), std::out_of_range);
-}
-
-TEST(ReducedDomainTest, InitializeWithRangeFromMiddle) {
-  // Even
-  using BaseDmn1 = dmn<4, unsigned int>;
-  BaseDmn1::set_elements(std::vector<unsigned int>{0, 10, 20, 30});
-
-  using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
-  ReducedDmn1::initialize(2, true);
-
-  const std::vector<unsigned int> check1{20, 30};
-  EXPECT_EQ(2, ReducedDmn1::get_size());
-  EXPECT_EQ(check1, ReducedDmn1::get_elements());
-  EXPECT_EQ("dmn<4, unsigned int>_reduced", ReducedDmn1::get_name());
-
-  // Odd
-  using BaseDmn2 = dmn<5, unsigned int>;
-  BaseDmn2::set_elements(std::vector<unsigned int>{0, 10, 20, 30, 40});
-
-  using ReducedDmn2 = dca::func::ReducedDomain<BaseDmn2>;
-  ReducedDmn2::initialize(3, true);
-
-  const std::vector<unsigned int> check2{20, 30, 40};
-  EXPECT_EQ(3, ReducedDmn2::get_size());
+  const std::vector<long> check2{0, 10, 20, 30};
+  EXPECT_EQ(4, ReducedDmn2::get_size());
   EXPECT_EQ(check2, ReducedDmn2::get_elements());
+  EXPECT_EQ("dmn<6, long>_reduced", ReducedDmn2::get_name());
 
-  // Zero
-  using BaseDmn3 = dmn<6, unsigned int>;
-  BaseDmn3::set_elements(std::vector<unsigned int>{0, 10, 20, 30, 40, 50});
+  // last = base-domain-size
+  using BaseDmn3 = dmn<7, long>;
+  BaseDmn3::set_elements(std::vector<long>{0, 10, 20, 30, 40, 50, 60});
 
   using ReducedDmn3 = dca::func::ReducedDomain<BaseDmn3>;
-  ReducedDmn3::initialize(0, true);
+  ReducedDmn3::initialize(5, 7);
 
-  EXPECT_EQ(0, ReducedDmn3::get_size());
-  EXPECT_EQ(std::vector<unsigned int>{}, ReducedDmn3::get_elements());
+  const std::vector<long> check3{50, 60};
+  EXPECT_EQ(2, ReducedDmn3::get_size());
+  EXPECT_EQ(check3, ReducedDmn3::get_elements());
+  EXPECT_EQ("dmn<7, long>_reduced", ReducedDmn3::get_name());
+
+  // first = last
+  using BaseDmn4 = dmn<8, long>;
+  BaseDmn4::set_elements(std::vector<long>{0, 10, 20, 30, 40, 50, 60, 70});
+
+  using ReducedDmn4 = dca::func::ReducedDomain<BaseDmn4>;
+  ReducedDmn4::initialize(3, 3);
+
+  EXPECT_EQ(0, ReducedDmn4::get_size());
+  EXPECT_EQ(std::vector<long>{}, ReducedDmn4::get_elements());
+  EXPECT_EQ("dmn<8, long>_reduced", ReducedDmn4::get_name());
 }
 
-TEST(ReducedDomainTest, InitializeWithRangeFromMiddleExceptions) {
-  // Range to copy goes beyond end of base domain
-  // Even
-  using BaseDmn1 = dmn<6, float>;
-  BaseDmn1::set_elements(std::vector<float>{-1., -2., -3., -4., -5., -6.});
+TEST(ReducedDomainTest, InitializeWithRangeExceptions) {
+  // first >= base-domain-size
+  using BaseDmn1 = dmn<6, double>;
+  BaseDmn1::set_elements(std::vector<double>{0., 10., 20., 30., 40., 50.});
 
   using ReducedDmn1 = dca::func::ReducedDomain<BaseDmn1>;
-  EXPECT_THROW(ReducedDmn1::initialize(4, true), std::out_of_range);
+  EXPECT_THROW(ReducedDmn1::initialize(6, 6), std::out_of_range);
 
-  // Odd
-  using BaseDmn2 = dmn<7, float>;
-  BaseDmn2::set_elements(std::vector<float>{-1., -2., -3., -4., -5., -6., -7.});
+  // last > base-domain-size
+  using BaseDmn2 = dmn<7, double>;
+  BaseDmn2::set_elements(std::vector<double>{0., 10., 20., 30., 40., 50., 60.});
 
   using ReducedDmn2 = dca::func::ReducedDomain<BaseDmn2>;
-  EXPECT_THROW(ReducedDmn2::initialize(5, true), std::out_of_range);
+  EXPECT_THROW(ReducedDmn2::initialize(1, 8), std::out_of_range);
+
+  // first > last
+  using BaseDmn3 = dmn<8, double>;
+  BaseDmn3::set_elements(std::vector<double>{0., 10., 20., 30., 40., 50., 60., 70.});
+
+  using ReducedDmn3 = dca::func::ReducedDomain<BaseDmn3>;
+  EXPECT_THROW(ReducedDmn3::initialize(2, 1), std::logic_error);
 }
