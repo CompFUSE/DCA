406dc1d87a4190917b3f01df442b65739ab85d9f
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-08-24 16:36:05 +0200

    Created validation and verification statistical tests for CT-AUX on a square lattice.

---
+++ test/integration/statistical_tests/square_lattice/ctaux_square_lattice_validation_stattest.cpp
@@ -0,0 +1,121 @@
+// Copyright (C) 2009-2016 ETH Zurich
+// Copyright (C) 2007?-2016 Center for Nanophase Materials Sciences, ORNL
+// All rights reserved.
+//
+// See LICENSE.txt for terms of usage.
+// See CITATION.txt for citation guidelines if you use this code for scientific publications.
+//
+// Author: Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
+//
+// Verification test of CT-AUX against a reference run
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include "gtest/gtest.h"
+
+#include "dca/math/statistical_testing/statistical_testing.hpp"
+#include "test/integration/statistical_tests/square_lattice/square_lattice_setup.hpp"
+
+dca::testing::DcaMpiTestEnvironment* dca_test_env;
+
+TEST(CtauxSquareLatticeValidationTest, GreensFunction) {
+  using namespace dca::testing;
+  const std::string ed_data_name = "data.ed.hdf5";
+
+  const int id = dca_test_env->concurrency.id();
+  const int number_of_samples = dca_test_env->concurrency.number_of_processors();
+
+  if (id == dca_test_env->concurrency.first()) {
+    dca::util::GitVersion::print();
+    dca::util::Modules::print();
+  }
+
+  ParametersType parameters(dca::util::GitVersion::string(), dca_test_env->concurrency);
+  parameters.read_input_and_broadcast<dca::io::JSONReader>(dca_test_env->input_file_name);
+  parameters.update_model();
+  parameters.update_domains();
+
+  DcaData data(parameters);
+  data.initialize();
+
+  // Do one QMC iteration
+  QuantumClusterSolver qmc_solver(parameters, data);
+  qmc_solver.initialize(0);
+  qmc_solver.integrate();
+
+  // stores quantities from integration.
+  using dca::func::function;
+  function<double, SigmaCutDomain> G_k_w_sample =
+      cutFrequency(qmc_solver.local_G_k_w(), n_frequencies);
+
+  auto G_k_w_avg(G_k_w_sample);
+  dca_test_env->concurrency.sum_and_average(G_k_w_avg);
+  G_k_w_avg.set_name("G_k_w");
+
+  // read the expected result
+  function<double, SigmaCutDomain> G_k_w_expected;
+
+  if (id == dca_test_env->concurrency.first()) {
+    function<std::complex<double>, SigmaDomain> G_k_w_full("cluster_greens_function_G_k_w");
+    dca::io::HDF5Reader reader;
+    reader.open_file(ed_data_name);
+    reader.open_group("functions");
+    reader.execute(G_k_w_full);
+    reader.close_group();
+    reader.close_file();
+    G_k_w_expected = cutFrequency(G_k_w_full, n_frequencies);
+  }
+  dca_test_env->concurrency.broadcast(G_k_w_expected);
+
+  // compute covariance and average ctin result.
+  function<double, CovarianceDomain> G_k_w_covariance("G_k_w_covariance");
+  dca_test_env->concurrency.computeCovariance(G_k_w_sample, G_k_w_avg, G_k_w_covariance);
+
+  //   compute p-value
+  if (id == dca_test_env->concurrency.first()) {
+    // read the stored reference data
+    dca::math::StatisticalTesting test(G_k_w_avg, G_k_w_expected, G_k_w_covariance, 1);
+    double p_value = test.computePValue(false, number_of_samples);
+    test.printInfo("ctaux_square_testinfo.out", true);
+    double p_value_default = 0.05;
+    std::cout << "\n***\nThe p-value is " << p_value << "\n***\n";
+    EXPECT_LT(p_value_default, p_value);
+  }
+
+  // write  integrator output
+  // INTERNAL: do we need it?
+  //  dca::phys::DcaLoopData<ParametersType> loop_data;
+  //  qmc_solver.finalize(loop_data);
+  //  if (id == 0) {
+  //    std::cout << "\nProcessor " << id << " is writing data " << std::endl;
+  //    dca::io::HDF5Writer writer;
+  //    writer.open_file("ctaux_square_results.hdf5");
+  //    writer.open_group("functions");
+  //    writer.execute(data.G_k_w);
+  //    writer.close_group();
+  //    writer.close_file();
+  //  }
+}
+
+int main(int argc, char** argv) {
+  int result = 0;
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  dca_test_env = new dca::testing::DcaMpiTestEnvironment(
+      argc, argv, dca::testing::test_directory + "square_lattice_input.json");
+  ::testing::AddGlobalTestEnvironment(dca_test_env);
+
+  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
+  if (dca_test_env->concurrency.id() != 0) {
+    delete listeners.Release(listeners.default_result_printer());
+    listeners.Append(new dca::testing::MinimalistPrinter);
+  }
+
+  result = RUN_ALL_TESTS();
+
+  return result;
+}
