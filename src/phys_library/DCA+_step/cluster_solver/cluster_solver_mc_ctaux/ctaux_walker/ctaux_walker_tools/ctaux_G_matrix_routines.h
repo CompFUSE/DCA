2b3669a1c9b993a466be5138e7bd868ea34f448b
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-08-31 16:56:50 +0200

    Fix usage of old/deprecated constructor signature.

--- src/phys_library/DCA+_step/cluster_solver/cluster_solver_mc_ctaux/ctaux_walker/ctaux_walker_tools/ctaux_G_matrix_routines.h
+++ src/phys_library/DCA+_step/cluster_solver/cluster_solver_mc_ctaux/ctaux_walker/ctaux_walker_tools/ctaux_G_matrix_routines.h
@@ -160,7 +160,7 @@ void G_TOOLS<device_t, parameters_type>::build_G_matrix(configuration_type& full
 #ifdef DCA_WITH_AUTOTUNING
     std::stringstream ss;
     ss << "GEMM_1_" << int(m / 16) * 16 + 8 << "_" << int(k / 16) * 16 + 8 << "_" << n;
-    profiler_t profiler_2(concurrency, ss.str().c_str(), __FILE__, __LINE__);
+    profiler_t profiler_2(ss.str().c_str(), __FILE__, __LINE__);
 #endif  // DCA_WITH_AUTOTUNING
 
     dca::linalg::UseDevice<device_t>::gemm("N", "N", m, n, k, 1., N.get_ptr(0, 0), LD_N,
