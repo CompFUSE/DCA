d38846e5b334b522fa260c33b8e5e480f12d7017
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-11-10 13:40:55 +0100

    Remove muldefs if-else blocks in CMakeLists.txt's.

--- cmake/dca_compiler_options.cmake
+++ cmake/dca_compiler_options.cmake
@@ -9,13 +9,13 @@
 # Check for --allow-multiple-definition linker flag.
 # References: https://cmake.org/pipermail/cmake/2011-July/045525.html
 #             http://projectsymphony.blogspot.ch/2013/03/cmake-linker-test-flags.html
-include(CheckCXXCompilerFlag)
-set(CMAKE_REQUIRED_FLAGS "-Wl,--allow-multiple-definition")
-check_cxx_compiler_flag("" DCA_HAVE_MULDEFS) # Pass empty string since we are testing a linker flag.
-if (DCA_HAVE_MULDEFS)
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--allow-multiple-definition")
-endif()
-unset(CMAKE_REQUIRED_FLAGS)
+# include(CheckCXXCompilerFlag)
+# set(CMAKE_REQUIRED_FLAGS "-Wl,--allow-multiple-definition")
+# check_cxx_compiler_flag("" DCA_HAVE_MULDEFS) # Pass empty string since we are testing a linker flag.
+# if (DCA_HAVE_MULDEFS)
+#   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--allow-multiple-definition")
+# endif()
+# unset(CMAKE_REQUIRED_FLAGS)
 
 # Warnings
 set(DCA_WARNINGS -Wall -Wextra -Wpedantic -Wno-sign-compare)
