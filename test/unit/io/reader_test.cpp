3911e1a25ad81cc2f4b606ff0c719471fa4cc974
Urs Haehner <haehneru@itp.phys.ethz.ch>
2016-12-26 15:25:32 +0100

    Add test for JSONReader reading vectors.

--- test/unit/io/reader_test.cpp
+++ test/unit/io/reader_test.cpp
@@ -7,11 +7,12 @@
 //
 // Author: Urs R. Haehner (haehneru@itp.phys.ethz.ch)
 //
-// For the moment this file just tests the construction of the different types of readers.
+// This file tests the readers of the IO library.
 
 #include "dca/io/csv/csv_reader.hpp"
 #include "dca/io/hdf5/hdf5_reader.hpp"
 #include "dca/io/json/json_reader.hpp"
+#include <vector>
 #include "gtest/gtest.h"
 #include "dca/util/ignore.hpp"
 
@@ -24,4 +25,23 @@ TYPED_TEST_CASE(ReaderTest, ReaderTypes);
 TYPED_TEST(ReaderTest, Constructor) {
   TypeParam reader;
   dca::util::ignoreUnused(reader);
+}
+
+TEST(JSONReaderTest, Vector) {
+  dca::io::JSONReader reader;
+  reader.open_file(DCA_SOURCE_DIR "/test/unit/io/vector.json");
+
+  // Simple 3D vector
+  std::vector<double> vec_1;
+  std::vector<double> vec_1_check{1., 2., 3.};
+  reader.execute("simple-vector", vec_1);
+  EXPECT_EQ(vec_1_check, vec_1);
+
+  // Vector of two 2D vectors
+  std::vector<std::vector<double>> vec_2;
+  std::vector<std::vector<double>> vec_2_check{{1.2, 3.4}, {5.6, 7.8}};
+  reader.execute("vector-of-vectors", vec_2);
+  EXPECT_EQ(vec_2_check, vec_2);
+
+  reader.close_file();
 }
