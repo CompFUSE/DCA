070c123f9f34b5631f3482d6f1ffa736244e550c
Giovanni <gbalduzz@itp.phys.ethz.ch>
2017-06-29 12:16:12 +0200

    Create cmake option to build statistical tests

--- cmake/dca_testing.cmake
+++ cmake/dca_testing.cmake
@@ -9,22 +9,25 @@ include(CMakeParseArguments)
 # Adds a test written with Google Test.
 #
 # dca_add_gtest(name
-#               [FAST | EXTENSIVE | PERFORMANCE]
+#               [FAST | EXTENSIVE | VALIDATION | PERFORMANCE]
+#               [STATISTICAL]
 #               [GTEST_MAIN]
 #               [MPI [MPI_NUMPROC procs]]
 #               [PTHREADS]
 #               [CUDA]
+#               [MAGMA]
 #               [INCLUDE_DIRS dir1 [dir2 ...]]
 #               [SOURCES src1 [src2 ...]]
 #               [LIBS lib1 [lib2 ...]])
 #
 # Adds a test called 'name', the source is assumed to be 'name.cpp'.
-# The type of the test can be FAST, EXTENSIVE or PERFORMANCE (mutually exclusive options). If no
+# The type of the test can be FAST, EXTENSIVE, VALIDATION or PERFORMANCE (mutually exclusive options). If no
 # option is specified, the default is FAST.
+# The test can be additionally marked as STATISTICAL if the result is not deterministic.
 # MPI, PTHREADS or CUDA may be given to indicate that the test requires these libraries. MPI_NUMPROC
 # is the number of MPI processes to use for an test with MPI, the default value is 1.
 function(dca_add_gtest name)
-  set(options FAST EXTENSIVE PERFORMANCE GTEST_MAIN MPI PTHREADS CUDA)
+  set(options FAST EXTENSIVE VALIDATION PERFORMANCE STATISTICAL GTEST_MAIN MPI PTHREADS CUDA MAGMA)
   set(oneValueArgs MPI_NUMPROC)
   set(multiValueArgs INCLUDE_DIRS SOURCES LIBS)
   cmake_parse_arguments(DCA_ADD_GTEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -32,11 +35,15 @@ function(dca_add_gtest name)
   # FAST, EXTENSIVE and PERFORMANCE are mutually exclusive.
   if ((DCA_ADD_GTEST_FAST AND DCA_ADD_GTEST_EXTENSIVE) OR
       (DCA_ADD_GTEST_FAST AND DCA_ADD_GTEST_PERFORMANCE) OR
-      (DCA_ADD_GTEST_EXTENSIVE AND DCA_ADD_GTEST_PERFORMANCE))
+      (DCA_ADD_GTEST_EXTENSIVE AND DCA_ADD_GTEST_PERFORMANCE) OR
+      (DCA_ADD_GTEST_VALIDATION AND DCA_ADD_GTEST_FAST) OR
+      (DCA_ADD_GTEST_VALIDATION AND DCA_ADD_GTEST_EXTENSIVE) OR
+      (DCA_ADD_GTEST_VALIDATION AND DCA_ADD_GTEST_PERFORMANCE))
     message(FATAL_ERROR "Incorrect use of dca_add_gtest.\n
                          dca_add_gtest(name\n
-                                       [FAST | EXTENSIVE | PERFORMANCE]\n
-                                       [GTEST_MAIN]
+                                       [FAST | EXTENSIVE | VALIDATION | PERFORMANCE]\n
+                                       [STATISTICAL]\n
+                                       [GTEST_MAIN]\n
                                        [MPI [MPI_NUMPROC procs]]\n
                                        [PTHREADS]\n
                                        [CUDA]\n
@@ -54,14 +61,25 @@ function(dca_add_gtest name)
     if (NOT (CMAKE_BUILD_TYPE STREQUAL "Release"))
       return ()
     endif()
-    
+
   elseif (DCA_ADD_GTEST_EXTENSIVE)
     if (NOT DCA_WITH_TESTS_EXTENSIVE)
       return()
     endif()
-    
+
+  elseif (DCA_ADD_GTEST_VALIDATION)
+    if (NOT DCA_WITH_TESTS_VALIDATION)
+      return()
+    endif()
+
   else()  # Default is FAST.
     if (NOT DCA_WITH_TESTS_FAST)
+      return()
+    endif()
+  endif()
+
+  if (DCA_ADD_GTEST_STATISTICAL)
+    if (NOT DCA_WITH_STATISTICAL_TESTING)
       return()
     endif()
   endif()
@@ -74,7 +92,7 @@ function(dca_add_gtest name)
   if (DCA_ADD_GTEST_PTHREADS AND NOT DCA_HAVE_PTHREADS)
     return()
   endif()
-  
+
   if (DCA_ADD_GTEST_CUDA AND NOT DCA_HAVE_CUDA)
     return()
   endif()
@@ -96,12 +114,12 @@ function(dca_add_gtest name)
   if (DCA_ADD_GTEST_PTHREADS)
     target_compile_definitions(${name} PRIVATE DCA_HAVE_PTHREADS)
   endif()
-  
+
   if (DCA_ADD_GTEST_CUDA)
     target_compile_definitions(${name} PRIVATE DCA_HAVE_CUDA)
     cuda_add_cublas_to_target(${name})
   endif()
-  
+
   target_include_directories(${name} PRIVATE
     ${gtest_SOURCE_DIR}/include
     ${DCA_ADD_GTEST_INCLUDE_DIRS})
