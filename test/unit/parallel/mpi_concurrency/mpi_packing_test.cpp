e290c24a0ae75d7fd1f99a07323e220857244b31
Urs Haehner <haehneru@itp.phys.ethz.ch>
2017-08-01 17:36:02 +0200

    Add failing test that demonstrates bug in MPIPacking::pack(dca::func::function).

--- test/unit/parallel/mpi_concurrency/mpi_packing_test.cpp
+++ test/unit/parallel/mpi_concurrency/mpi_packing_test.cpp
@@ -36,6 +36,63 @@ TEST(MPIPackingTest, GetBufferSizeFunction) {
   EXPECT_EQ(36, packing.get_buffer_size(f_const));
 }
 
+TEST(MPIPackingTest, PackAndUnpackFunction) {
+  dca::parallel::MPIProcessorGrouping grouping;
+  grouping.set();
+
+  dca::parallel::MPIPacking packing(grouping);
+
+  using TestDomain = dca::func::dmn_0<dca::func::dmn<4, int>>;
+
+  dca::func::function<double, TestDomain> f("test-function");
+  for (std::size_t i = 0; i < f.size(); ++i)
+    f(i) = i + 3.14;
+
+  //
+  // Non-const function
+  //
+  dca::func::function<double, TestDomain> f_non_const(f, "non-const-function");
+
+  const auto buffer_size_non_const = packing.get_buffer_size(f_non_const);
+  int* buffer_non_const = new int[buffer_size_non_const];
+  int offset_non_const = 0;
+
+  packing.pack(buffer_non_const, buffer_size_non_const, offset_non_const, f_non_const);
+
+  EXPECT_EQ(buffer_size_non_const, offset_non_const);
+  for (std::size_t i = 0; i < f.size(); ++i)
+    EXPECT_EQ(f(i), f_non_const(i));
+
+  dca::func::function<double, TestDomain> f_unpacked("unpacked");
+  int offset_unpack = 0;
+
+  packing.unpack(buffer_non_const, buffer_size_non_const, offset_unpack, f_unpacked);
+
+  EXPECT_EQ(buffer_size_non_const, offset_unpack);
+  for (std::size_t i = 0; i < f.size(); ++i)
+    EXPECT_EQ(f(i), f_unpacked(i));
+
+  //
+  // Const function
+  //
+  const dca::func::function<double, TestDomain> f_const(f, "const-function");
+
+  const auto buffer_size_const = packing.get_buffer_size(f_const);
+  int* buffer_const = new int[buffer_size_const];
+  int offset_const = 0;
+
+  packing.pack(buffer_const, buffer_size_const, offset_const, f_const);
+
+  EXPECT_EQ(buffer_size_const, offset_const);
+
+  offset_unpack = 0;
+  packing.unpack(buffer_const, buffer_size_const, offset_unpack, f_unpacked);
+
+  EXPECT_EQ(buffer_size_const, offset_unpack);
+  for (std::size_t i = 0; i < f_const.size(); ++i)
+    EXPECT_EQ(f_const(i), f_unpacked(i));
+}
+
 int main(int argc, char** argv) {
   int result = 0;
 
