4f0badeb4b59528ebbf48862ae5f1470211a1925
Urs Haehner <haehneru@itp.phys.ethz.ch>
2017-08-18 12:29:52 +0200

    Remove get_buffer_size test since it is system and MPI implementation dependent.

--- test/unit/parallel/mpi_concurrency/mpi_packing_test.cpp
+++ test/unit/parallel/mpi_concurrency/mpi_packing_test.cpp
@@ -18,24 +18,6 @@
 #include "dca/function/function.hpp"
 #include "dca/testing/minimalist_printer.hpp"
 
-TEST(MPIPackingTest, GetBufferSizeFunction) {
-  dca::parallel::MPIProcessorGrouping grouping;
-  grouping.set();
-
-  dca::parallel::MPIPacking packing(grouping);
-
-  using TestDomain = dca::func::dmn_0<dca::func::dmn<4, int>>;
-
-  // Non-const function
-  dca::func::function<double, TestDomain> f_non_const("non-const-function");
-  // 1 int (number of elements) + 4 doubles (elements) = 36 bytes.
-  EXPECT_EQ(36, packing.get_buffer_size(f_non_const));
-
-  // Const function
-  const dca::func::function<double, TestDomain> f_const("const-function");
-  EXPECT_EQ(36, packing.get_buffer_size(f_const));
-}
-
 TEST(MPIPackingTest, PackAndUnpackFunction) {
   dca::parallel::MPIProcessorGrouping grouping;
   grouping.set();
