7780c35363c4e72673a3cae4a21a969476b1ed51
Giovanni <gbalduzz@itp.phys.ethz.ch>
2018-02-22 16:52:27 +0100

    Added more operators to complex_operators_cuda.cu.hpp

--- include/dca/linalg/util/complex_operators_cuda.cu.hpp
+++ include/dca/linalg/util/complex_operators_cuda.cu.hpp
@@ -30,6 +30,33 @@ __device__ __host__ static __inline__ cuComplex operator*(const cuComplex a, con
 __device__ __host__ static __inline__ cuComplex operator/(const cuComplex a, const cuComplex b) {
   return cuCdivf(a, b);
 }
+__device__ __host__ static __inline__ cuComplex operator-(cuComplex a) {
+  a.x = -a.x;
+  a.y = -a.y;
+  return a;
+}
+__device__ __host__ static __inline__ cuComplex operator*(const cuComplex a, const float b) {
+  return make_cuComplex(a.x * b, a.y * b);
+}
+__device__ __host__ static __inline__ cuComplex operator*=(
+    cuComplex& a, const cuComplex b) {
+  a = a * b;
+  return a;
+}
+__device__ __host__ static __inline__ cuComplex operator+=(
+    cuComplex& a, const cuComplex b) {
+  a = a + b;
+  return a;
+}
+__device__ __host__ static __inline__ cuComplex operator-=(
+    cuComplex& a, const cuComplex b) {
+  a = a - b;
+  return a;
+}
+__device__ __host__ static __inline__ cuComplex conj(cuComplex a){
+  a.y = -a.y;
+  return a;
+}
 
 __device__ __host__ static __inline__ cuDoubleComplex operator+(const cuDoubleComplex a,
                                                                 const cuDoubleComplex b) {
@@ -46,6 +73,35 @@ __device__ __host__ static __inline__ cuDoubleComplex operator*(const cuDoubleCo
 __device__ __host__ static __inline__ cuDoubleComplex operator/(const cuDoubleComplex a,
                                                                 const cuDoubleComplex b) {
   return cuCdiv(a, b);
+}
+
+__device__ __host__ static __inline__ cuDoubleComplex operator-(cuDoubleComplex a) {
+  a.x = -a.x;
+  a.y = -a.y;
+  return a;
+}
+__device__ __host__ static __inline__ cuDoubleComplex operator*(const cuDoubleComplex a,
+                                                                const double b) {
+  return make_cuDoubleComplex(a.x * b, a.y * b);
+}
+__device__ __host__ static __inline__ cuDoubleComplex operator*=(
+    cuDoubleComplex& a, const cuDoubleComplex b) {
+  a = a * b;
+  return a;
+}
+__device__ __host__ static __inline__ cuDoubleComplex operator+=(
+    cuDoubleComplex& a, const cuDoubleComplex b) {
+  a = a + b;
+  return a;
+}
+__device__ __host__ static __inline__ cuDoubleComplex operator-=(
+    cuDoubleComplex& a, const cuDoubleComplex b) {
+  a = a - b;
+  return a;
+}
+__device__ __host__ static __inline__ cuDoubleComplex conj(cuDoubleComplex a){
+  a.y = -a.y;
+  return a;
 }
 
 }  // linalg
