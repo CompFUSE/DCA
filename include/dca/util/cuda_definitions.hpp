b5cad21e5df886bba8f4f56711a8c304288bbad9
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-03-26 15:48:40 +0200

    cuda definitions.

---
+++ include/dca/util/cuda_definitions.hpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2009-2016 ETH Zurich
+// Copyright (C) 2007?-2016 Center for Nanophase Materials Sciences, ORNL
+// All rights reserved.
+// See LICENSE.txt for terms of usage./
+// See CITATION.txt for citation guidelines if you use this code for scientific publications.
+//
+// Author: Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
+//
+// Redefine keyword like __device__ and __host__
+// so that code can be compiled either by nvcc or the host compiler.
+
+#ifndef DCA_UTIL_CUDA_DEFINITIONS_HPP
+#define DCA_UTIL_CUDA_DEFINITIONS_HPP
+
+#ifdef __CUDACC__
+#define __HOST__ __host__
+#define __DEVICE__ __device__
+#define IS_CUDA_COMPILER
+
+#else // the compiler is not nvcc
+#define __HOST__
+#define __DEVICE__
+#endif
+
+
+#endif  // DCA_UTIL_CUDA_DEFINITIONS_HPP
