2d20aa511c9d87240957604868b1e50061491721
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-03-28 16:37:22 +0200

    implemented SFINAE for linalg::util::synchronizeStream(int, int)

--- include/dca/linalg/util/stream_functions.hpp
+++ include/dca/linalg/util/stream_functions.hpp
@@ -12,14 +12,18 @@
 #ifndef DCA_LINALG_UTIL_STREAM_FUNCTIONS_HPP
 #define DCA_LINALG_UTIL_STREAM_FUNCTIONS_HPP
 
+#ifdef DCA_HAVE_CUDA
 #include <cuda_runtime.h>
 #include "dca/linalg/util/def.hpp"
 #include "dca/linalg/util/stream_container.hpp"
+#endif  // DCA_HAVE_CUDA
 
 namespace dca {
 namespace linalg {
 namespace util {
 // dca::linalg::util::
+
+#ifdef DCA_HAVE_CUDA
 
 inline StreamContainer<DCA_MAX_THREADS, DCA_STREAMS_PER_THREAD>& getStreamContainer() {
   static StreamContainer<DCA_MAX_THREADS, DCA_STREAMS_PER_THREAD> stream_container;
@@ -37,6 +41,13 @@ inline cudaStream_t getStream(int thread_id, int stream_id) {
 inline void syncStream(int thread_id, int stream_id) {
   getStreamContainer().sync(thread_id, stream_id);
 }
+
+#else
+
+// SFINAE version of syncStream.
+inline void syncStream(int /*thread_id*/, int /*stream_id*/) {}
+
+#endif  // DCA_HAVE_CUDA
 
 }  // util
 }  // linalg
