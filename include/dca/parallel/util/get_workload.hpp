084ae89d1a3617af5ed07054ac69a61798f6653a
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-04-03 16:14:02 +0200

    created util to compute the local workload

---
+++ include/dca/parallel/util/get_workload.hpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2009-2016 ETH Zurich
+// Copyright (C) 2007?-2016 Center for Nanophase Materials Sciences, ORNL
+// All rights reserved.
+//
+// See LICENSE.txt for terms of usage.
+// See CITATION.txt for citation guidelines if you use this code for scientific publications.
+//
+// Author: Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
+//
+// This file provides a function that computes the local work size given local id and total amount
+// of work.
+
+#ifndef DCA_PARALLEL_UTIL_GET_WORKLOAD_HPP
+#define DCA_PARALLEL_UTIL_GET_WORKLOAD_HPP
+
+#include <cassert>
+
+namespace dca {
+namespace parallel {
+namespace util {
+// dca::parallel::util::
+
+template <class Concurrency>
+int getWorkload(const uint total_work, const uint n_local_workers, const uint local_id,
+                const Concurrency& concurrency) {
+  assert(local_id < n_local_workers);
+
+  const int n_workers = n_local_workers * concurrency.number_of_processors();
+  const int id = local_id + n_local_workers * concurrency.id();
+
+  int work = total_work / n_workers;
+  if (id < total_work % n_workers)
+    ++work;
+  return work;
+}
+
+}  // util
+}  // parallel
+}  // dca
+
+#endif  // DCA_PARALLEL_UTIL_GET_WORKLOAD_HPP
