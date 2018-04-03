da4e7425800bba1e784d8b2b638e711239afe56e
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-04-03 11:47:06 +0200

    Changed MPITypeMap implementation to a completely generic one. Does not support heterogeneous types.

--- include/dca/parallel/mpi_concurrency/mpi_type_map.hpp
+++ include/dca/parallel/mpi_concurrency/mpi_type_map.hpp
@@ -7,120 +7,28 @@
 //
 // Author: Peter Staar (taa@zurich.ibm.com)
 //         Urs R. Haehner (haehneru@itp.phys.ethz.ch)
+//         Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
 //
-// This class maps C++ types to MPI types.
-//
-// TODO: Check the MPI types.
+// This class provides an utility for determining the size of a C++ object during MPI
+// communications.
 
 #ifndef DCA_PARALLEL_MPI_CONCURRENCY_MPI_TYPE_MAP_HPP
 #define DCA_PARALLEL_MPI_CONCURRENCY_MPI_TYPE_MAP_HPP
 
-#include <complex>
-#include <cstdlib>
 #include <mpi.h>
 
 namespace dca {
 namespace parallel {
 // dca::parallel::
 
-// Empty class template that causes a compile error when a type without template specialization is
-// used.
-template <typename scalar_type>
-class MPITypeMap {};
-
-template <>
-class MPITypeMap<bool> {
-public:
+template <typename T>
+struct MPITypeMap {
   static std::size_t factor() {
-    return 1;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_CXX_BOOL;
-  }
-};
-
-template <>
-class MPITypeMap<char> {
-public:
-  static std::size_t factor() {
-    return 1;
+    return sizeof(T);
   }
 
   static MPI_Datatype value() {
     return MPI_CHAR;
-  }
-};
-
-template <>
-class MPITypeMap<int> {
-public:
-  static std::size_t factor() {
-    return 1;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_INT;
-  }
-};
-
-template <>
-class MPITypeMap<std::size_t> {
-public:
-  static std::size_t factor() {
-    return 1;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_UNSIGNED_LONG;
-  }
-};
-
-template <>
-class MPITypeMap<float> {
-public:
-  static std::size_t factor() {
-    return 1;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_FLOAT;
-  }
-};
-
-template <>
-class MPITypeMap<double> {
-public:
-  static std::size_t factor() {
-    return 1;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_DOUBLE;
-  }
-};
-
-template <>
-class MPITypeMap<std::complex<float>> {
-public:
-  static std::size_t factor() {
-    return 2;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_FLOAT;
-  }
-};
-
-template <>
-class MPITypeMap<std::complex<double>> {
-public:
-  static std::size_t factor() {
-    return 2;
-  }
-
-  static MPI_Datatype value() {
-    return MPI_DOUBLE;
   }
 };
 
