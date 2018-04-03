0b7e3f0b2bf47bbae942dbf6a2aa42f234f4e4cc
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-04-03 13:35:20 +0200

    Removed references of ct-int from test_setup.hpp.

--- test/unit/phys/dca_step/cluster_solver/test_setup.hpp
+++ test/unit/phys/dca_step/cluster_solver/test_setup.hpp
@@ -17,15 +17,12 @@
 
 #include "dca/io/json/json_reader.hpp"
 #include "dca/phys/parameters/parameters.hpp"
-// INTERNAL: to be added with CT_INT solver
-//#include "dca/phys/dca_step/cluster_solver/ctint/structs/interaction_vertices.hpp"
 #include "dca/phys/domains/cluster/symmetries/point_groups/2d/2d_square.hpp"
 #include "dca/phys/domains/quantum/electron_band_domain.hpp"
 #include "dca/phys/domains/quantum/electron_spin_domain.hpp"
 #include "dca/phys/domains/time_and_frequency/frequency_domain.hpp"
 #include "dca/phys/models/analytic_hamiltonians/square_lattice.hpp"
 #include "dca/phys/models/analytic_hamiltonians/bilayer_lattice.hpp"
-//#include "dca/phys/models/analytic_hamiltonians/hund_lattice.hpp"
 #include "dca/parallel/no_concurrency/no_concurrency.hpp"
 #include "dca/parallel/no_threading/no_threading.hpp"
 #include "dca/phys/dca_data/dca_data.hpp"
@@ -38,7 +35,6 @@ namespace testing {
 
 using LatticeSquare = phys::models::square_lattice<phys::domains::D4>;
 using LatticeBilayer = phys::models::bilayer_lattice<phys::domains::D4>;
-// using LatticeHund = phys::models::HundLattice<phys::domains::D4>;
 
 template <class Lattice = LatticeSquare, phys::solver::ClusterSolverName solver_name = phys::solver::CT_AUX>
 struct G0Setup : public ::testing::Test {
@@ -61,7 +57,6 @@ struct G0Setup : public ::testing::Test {
   Concurrency concurrency;
   Parameters parameters;
   std::unique_ptr<Data> data;
-  //  phys::solver::ctint::InteractionVertices interaction_vertices;
 
   G0Setup() : concurrency(0, nullptr), parameters("", concurrency) {}
 
@@ -75,10 +70,6 @@ struct G0Setup : public ::testing::Test {
 
     data.reset(new Data(parameters));
     data->initialize();
-
-    //    interaction_vertices.initializeFromHamiltonian(data->H_interactions);
-    //    if (data->has_non_density_interactions())
-    //      interaction_vertices.initializeFromNonDensityHamiltonian(data->get_non_density_interactions());
   }
 
   virtual void TearDown() {}
