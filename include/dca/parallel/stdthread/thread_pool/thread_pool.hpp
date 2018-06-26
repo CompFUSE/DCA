f51ffac6f43774467bf2da7d3b789a4c7c41a671
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-06-26 10:27:05 +0200

    Use a different queue per thread. No work stealing.

--- include/dca/parallel/stdthread/thread_pool/thread_pool.hpp
+++ include/dca/parallel/stdthread/thread_pool/thread_pool.hpp
@@ -16,6 +16,7 @@
 #define DCA_PARALLEL_STDTHREAD_THREAD_POOL_THREAD_POOL_HPP
 
 #include <condition_variable>
+#include <iostream>
 #include <functional>
 #include <future>
 #include <memory>
@@ -43,34 +44,40 @@ private:
   // need to keep track of threads so we can join them
   std::vector<std::thread> workers_;
   // the task queue
-  std::queue<std::packaged_task<void()>> tasks_;
+  std::vector<std::queue<std::packaged_task<void()>>> tasks_;
 
   // synchronization
-  std::mutex queue_mutex_;
-  std::condition_variable condition_;
-  bool stop_;
+  std::vector<std::mutex> queue_mutex_;
+  std::vector<std::condition_variable> condition_;
+  std::atomic<bool> stop_;
+  unsigned int active_id_;
 };
 
 // the constructor just launches some amount of workers_
-inline ThreadPool::ThreadPool(size_t n_threads) : stop_(false) {
+inline ThreadPool::ThreadPool(size_t n_threads)
+    : tasks_(n_threads), queue_mutex_(n_threads), condition_(n_threads), stop_(false), active_id_(0) {
   for (size_t i = 0; i < n_threads; ++i)
-    workers_.emplace_back([this] {
-      while (true) {
-        std::packaged_task<void()> task;
+    // Start a loop on a different thread.
+    workers_.emplace_back(
+        [this](const int id) {
+          while (true) {
+            std::packaged_task<void()> task;
 
-        {  // Acquire new task.
-          std::unique_lock<std::mutex> lock(this->queue_mutex_);
-          this->condition_.wait(lock, [this] { return this->stop_ || !this->tasks_.empty(); });
-          // If all the work is done and no more will be posted, return.
-          if (this->stop_ && this->tasks_.empty())
-            return;
-          task = std::move(this->tasks_.front());
-          this->tasks_.pop();
-        }
-        // Execute task.
-        task();
-      }
-    });
+            {  // Acquire new task.
+              std::unique_lock<std::mutex> lock(this->queue_mutex_[id]);
+              this->condition_[id].wait(
+                  lock, [this, id] { return this->stop_ || !this->tasks_[id].empty(); });
+              // If all the work is done and no more will be posted, return.
+              if (this->stop_ && this->tasks_[id].empty())
+                return;
+              task = std::move(this->tasks_[id].front());
+              this->tasks_[id].pop();
+            }
+            // Execute task.
+            task();
+          }
+        },
+        i);
 }
 
 // add new work item to the pool
@@ -78,31 +85,32 @@ template <class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
     -> std::future<typename std::result_of<F(Args...)>::type> {
   using return_type = typename std::result_of<F(Args...)>::type;
+  const int id = active_id_;
 
   auto task =
       std::packaged_task<return_type()>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
   std::future<return_type> res = task.get_future();
   {
-    std::unique_lock<std::mutex> lock(queue_mutex_);
+    std::unique_lock<std::mutex> lock(queue_mutex_[id]);
 
     // don't allow enqueueing after stopping the pool
     if (stop_)
       throw std::runtime_error("enqueue on stopped ThreadPool");
 
-    tasks_.emplace(std::move(task));
+    tasks_[id].emplace(std::move(task));
   }
-  condition_.notify_one();
+  condition_[id].notify_one();
+
+  active_id_ = (active_id_ + 1) % size();
   return res;
 }
 
 // the destructor joins all threads
 inline ThreadPool::~ThreadPool() {
-  {
-    std::unique_lock<std::mutex> lock(queue_mutex_);
-    stop_ = true;
-  }
-  condition_.notify_all();
+  stop_ = true;
+  for (auto& condition : condition_)
+    condition.notify_one();
   for (std::thread& worker : workers_)
     worker.join();
 }
