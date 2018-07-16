d3dfc2b87b1642d280356261a13b3c38eabca654
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-07-16 14:13:57 -0400

    removed easybuild reference in the titan script.

--- build-aux/titan.cmake
+++ build-aux/titan.cmake
@@ -12,10 +12,11 @@ set(TEST_RUNNER "aprun" CACHE STRING "Command for executing (MPI) programs.")
 # Enable the GPU support.
 option(DCA_WITH_CUDA "Enable GPU support." ON)
 
-# Compile for Kepler compute architecture.
-set(CUDA_GPU_ARCH "sm_35" CACHE STRING "Name of the *real* architecture to build for.")
 
 # For the GPU support we also need MAGMA.
 # MAGMA has been installed with EasyBuild.
-set(MAGMA_DIR $ENV{EBROOTMAGMA} CACHE PATH
+set(MAGMA_DIR $ENV{OLCF_ROOT_MAGMA} CACHE PATH
   "Path to the MAGMA installation directory. Hint for CMake to find MAGMA.")
+
+set(FFTW_DIR $ENV{OLCF_ROOT_FFTW} CACHE PATH
+        "Path to the FFTW3 installation directory. Hint for CMake to find FFTW3.")
