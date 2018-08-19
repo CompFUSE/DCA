cd438ef7f8de2eed3e24e71a54e99cffeac57ddd
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-08-19 09:38:36 -0400

    added failing test to hdf5 reader test relative to a constant vector.

--- test/unit/io/hdf5_reader_test.cpp
+++ test/unit/io/hdf5_reader_test.cpp
@@ -6,6 +6,7 @@
 // See CITATION.md for citation guidelines, if DCA++ is used for scientific publications.
 //
 // Author: Urs R. Haehner (haehneru@itp.phys.ethz.ch)
+//         Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
 //
 // This file provides specific tests for the HDF5 reader.
 
@@ -41,4 +42,32 @@ TEST(HDF5ReaderTest, DestructorCleanUp) {
 
   // Destructor closes file.
   // reader.close_file();
+}
+
+TEST(HDF5ReaderTest, VectorReadWrite) {
+  const std::string object_name = "a_vector";
+  const std::string file_name = "hdf5_reader_vector_test.hdf5";
+  const std::vector<std::complex<double>> a_vector{
+      std::complex<double>(1., 0.), std::complex<double>(0., 1.), std::complex<double>(23.4, -1.5)};
+
+  // Create test file.
+  dca::io::HDF5Writer writer;
+  writer.open_file(file_name);
+  writer.execute(object_name, a_vector);
+  writer.close_file();
+
+
+  // Read test file.
+  dca::io::HDF5Reader reader;
+  std::vector<std::complex<double>> vector_read;
+  reader.open_file(file_name);
+  reader.execute(object_name, vector_read);
+
+  ASSERT_EQ(a_vector.size(), vector_read.size());
+  for(int i =0; i < a_vector.size(); ++i) {
+    EXPECT_DOUBLE_EQ(std::real(a_vector[i]), std::real(vector_read[i]));
+    EXPECT_DOUBLE_EQ(std::imag(a_vector[i]), std::imag(vector_read[i]));
+  }
+
+  reader.close_file();
 }
