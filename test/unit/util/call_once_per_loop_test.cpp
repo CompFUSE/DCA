ec357c96f2d6b4125de663d93837533fa99fe54f
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-09-29 12:26:34 +0200

    Revert "Remove mutex lock from call_once_per_loop"
    This reverts commit 547927d80d0893c90d4f8094e037eee41ae92724.

--- test/unit/util/call_once_per_loop_test.cpp
+++ test/unit/util/call_once_per_loop_test.cpp
@@ -18,34 +18,21 @@
 
 void task(uint loop_id, std::vector<int>& data) {
   static dca::util::OncePerLoopFlag flag;
-  // to prevent optimizer removing dummy loop
-  static volatile int dummy = 0;
 
-  dca::util::callOncePerLoop(flag, loop_id, [&]()
-    {
-      ++data[loop_id];
-      for (int i=0; i<1000; i++) {
-        for (int j=0; j<1000; j++) {
-          dummy++;
-        }
-      }
-    }
-  );
+  dca::util::callOncePerLoop(flag, loop_id, [&]() { ++data[loop_id]; });
 }
 
 TEST(CallOncePerLoopTest, All) {
-  const int n_loops(1000);
+  const int n_loops(10);
   std::vector<int> result(n_loops, 0);
 
   {
-    const int n_threads = 8;
+    const int n_threads = 4;
     dca::parallel::ThreadPool pool(n_threads);
 
-    for (uint loop_id = 0; loop_id < n_loops; ++loop_id) {
-      for (int thread_id = 0; thread_id < n_threads; ++thread_id) {
+    for (uint loop_id = 0; loop_id < n_loops; ++loop_id)
+      for (int thread_id = 0; thread_id < n_loops; ++thread_id)
         pool.enqueue(task, loop_id, std::ref(result));
-      }
-    }
   }
 
   for (int elem : result)
