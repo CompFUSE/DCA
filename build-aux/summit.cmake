bb8d054ed4e2a1f77341f9028acb255c458d08e2
Li, Ying Wai <yingwaili@ornl.gov>
2018-12-06 16:39:51 -0500

    More modifications to summit.cmake: 1. Add search path to the BLAS library in LAPACK_LIBRARIES. 2. Use "mxm" instead of "tcp" in the --smpiargs flag for MPI tests to pass. It is supposed to be a temporary fix and not intended to be a permanent solution.

--- build-aux/summit.cmake
+++ build-aux/summit.cmake
@@ -6,8 +6,7 @@
 # Paths to IBM's ESSL (preferred) and NETLIB-LAPACK will be set manually.
 set(DCA_HAVE_LAPACK TRUE CACHE INTERNAL "If set to TRUE, prevents CMake from searching for LAPACK.")
 # To give ESSL precedence it needs to be specified before NETLIB.
-set(LAPACK_LIBRARIES $ENV{OLCF_ESSL_ROOT}/lib64/libessl.so;$ENV{OLCF_NETLIB_LAPACK_ROOT}/lib64/liblapack.so CACHE FILEPATH
-    "Libraries to link against to use LAPACK.")
+set(LAPACK_LIBRARIES $ENV{OLCF_ESSL_ROOT}/lib64/libessl.so;$ENV{OLCF_NETLIB_LAPACK_ROOT}/lib64/liblapack.so;$ENV{OLCF_NETLIB_LAPACK_ROOT}/lib64/libblas.so CACHE FILEPATH "Libraries to link against to use LAPACK.")
 
 # Use jsrun for executing the tests.
 set(TEST_RUNNER "jsrun" CACHE STRING "Command for executing (MPI) programs.")
@@ -18,9 +17,10 @@ set(MPIEXEC_PREFLAGS "-n 1 -g 1 -c 8" CACHE STRING
   "Flags to pass to TEST_RUNNER directly before the executable to run.")
 # The flag "--smpiargs=none" is needed to execute tests with no MPI functionalities.
 set(SMPIARGS_FLAG_NOMPI "--smpiargs=none" CACHE STRING
-    "Spectrum MPI argument list flag for serial tests.")
-# The flag "--smpiargs=-tcp" is needed to execute tests with MPI.
-set(SMPIARGS_FLAG_MPI "--smpiargs=-tcp" CACHE STRING "Spectrum MPI argument list flag for MPI tests.")
+  "Spectrum MPI argument list flag for serial tests.")
+# The flag "--smpiargs=-mxm" is a workaround (for now) to execute tests with MPI to avoid tests from failing.
+# It uses Mellanox's protocol and bypasses PAMI for MPI communications.
+set(SMPIARGS_FLAG_MPI "--smpiargs=-mxm" CACHE STRING "Spectrum MPI argument list flag for MPI tests.")
 
 # Enable the GPU support.
 option(DCA_WITH_CUDA "Enable GPU support." ON)
