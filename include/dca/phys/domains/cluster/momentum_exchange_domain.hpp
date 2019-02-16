f349434cf42401e96d96d509c8ced21d48ea740c
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2019-02-15 22:51:20 -0500

    Use only independent transfer k points.

--- include/dca/phys/domains/cluster/momentum_exchange_domain.hpp
+++ include/dca/phys/domains/cluster/momentum_exchange_domain.hpp
@@ -13,9 +13,12 @@
 #ifndef DCA_PHYS_DOMAINS_CLUSTER_CLUSTER_CLUSTER_DOMAIN_HPP
 #define DCA_PHYS_DOMAINS_CLUSTER_CLUSTER_CLUSTER_DOMAIN_HPP
 
+#include <array>
 #include <cassert>
 #include <string>
 #include <vector>
+
+#include "dca/phys/domains/cluster/cluster_symmetry.hpp"
 
 namespace dca {
 namespace phys {
@@ -29,7 +32,7 @@ public:
 
   // Initialize size and elements.
   template <class Parameters>
-  static void initialize(const Parameters& parameters);
+  static void initialize(const Parameters& parameters, bool verbose = false);
 
   // Returns the number of computed momentum exchanges.
   // Precondition: the domain is initialized.
@@ -59,7 +62,9 @@ public:
   static void write(Writer& writer);
 
 private:
-  static void initialize(bool compute_all_transfers, int transfer_index, int cluster_size);
+  static void initialize(bool compute_all_transfers, int transfer_index,
+                         const std::vector<std::vector<double>>& cluster_elements,
+                         const std::vector<std::array<int, 2>>& symmetries, bool verbose);
 
 private:
   static std::vector<int> elements_;
@@ -67,9 +72,23 @@ private:
 };
 
 template <class Parameters>
-void MomentumExchangeDomain::initialize(const Parameters& parameters) {
+void MomentumExchangeDomain::initialize(const Parameters& parameters, bool verbose) {
+  using KDmn = typename Parameters::KClusterDmn::parameter_type;
+  using SymmetryDmn = typename domains::cluster_symmetry<KDmn>;
+  const auto& symmetry_table = SymmetryDmn::get_symmetry_matrix();
+  assert(KDmn::get_size() == symmetry_table[0]);
+
+  // TODO: check for multibands.
+  std::vector<std::array<int, 2>> symmetries;
+  for (int k1 = 0; k1 < KDmn::get_size(); ++k1)
+    for (int sim = 0; sim < symmetry_table[2]; ++sim) {
+      const int k2 = symmetry_table(k1, 0, sim).first;
+      if (k1 != k2)
+        symmetries.push_back(std::array<int, 2>{k1, k2});
+    }
+
   initialize(parameters.compute_all_transfers(), parameters.get_four_point_momentum_transfer_index(),
-             Parameters::KClusterDmn::dmn_size());
+             KDmn::get_elements(), symmetries, verbose);
 }
 
 template <class Writer>
