785edf6655e5b6c12d8e584db5f0112b26f08651
Urs Haehner <haehneru@itp.phys.ethz.ch>
2019-03-04 14:30:33 +0100

    Finish test of computation of bare dual Green's function.

--- test/unit/phys/dual_fermion/compute_bare_dual_greens_function_test.cpp
+++ test/unit/phys/dual_fermion/compute_bare_dual_greens_function_test.cpp
@@ -36,27 +36,70 @@ protected:
 };
 
 TEST_F(ComputeBareDualGreensFunctionTest, TwoClusterSites) {
-  func::function<std::complex<double>,
-                 func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, func::dmn_0<KClusterDmn>>>
-      eps_cg;
+  func::function<std::complex<double>, func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, KClusterDmn>> eps_cg;
+  const double eps_cg_0 = 42.;
+  const double eps_cg_1 = 99.;
+  for (int s = 0; s < OrbitalSpinDmn::dmn_size(); ++s) {
+    eps_cg(s, s, 0) = eps_cg_0;
+    eps_cg(s, s, 1) = eps_cg_1;
+  }
 
   func::function<std::complex<double>, func::dmn_variadic<KClusterDmn, KClusterDmn, KSuperlatticeDmn>>
       eps_tilde;
+  for (int k = 0; k < KSuperlatticeDmn::dmn_size(); ++k) {
+    eps_tilde(0, 0, k) = 1.;
+    eps_tilde(0, 1, k) = 2.;
+    eps_tilde(1, 0, k) = 3.;
+    eps_tilde(1, 1, k) = 4.;
+  }
 
   func::function<std::complex<double>,
-                 func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, func::dmn_0<KClusterDmn>,
-                                    func::dmn_0<MatsubaraFreqDmn>>>
+                 func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, KClusterDmn, MatsubaraFreqDmn>>
       Delta;
+  const double Delta_0 = 1.23;
+  const double Delta_1 = 4.56;
+  for (int w = 0; w < MatsubaraFreqDmn::dmn_size(); ++w)
+    for (int s = 0; s < OrbitalSpinDmn::dmn_size(); ++s) {
+      Delta(s, s, 0, w) = Delta_0;
+      Delta(s, s, 1, w) = Delta_1;
+    }
 
   func::function<std::complex<double>,
-                 func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, func::dmn_0<KClusterDmn>,
-                                    func::dmn_0<MatsubaraFreqDmn>>>
+                 func::dmn_variadic<OrbitalSpinDmn, OrbitalSpinDmn, KClusterDmn, MatsubaraFreqDmn>>
       G;
+  const double G_0 = 3.14;
+  const double G_1 = 2.72;
+  for (int w = 0; w < MatsubaraFreqDmn::dmn_size(); ++w)
+    for (int s = 0; s < OrbitalSpinDmn::dmn_size(); ++s) {
+      G(s, s, 0, w) = G_0;
+      G(s, s, 1, w) = G_1;
+    }
 
-  func::function<std::complex<double>, func::dmn_variadic<KClusterDmn, KClusterDmn, KSuperlatticeDmn,
-                                                          func::dmn_0<MatsubaraFreqDmn>>>
+  func::function<std::complex<double>,
+                 func::dmn_variadic<KClusterDmn, KClusterDmn, KSuperlatticeDmn, MatsubaraFreqDmn>>
 
       G0_tilde;
 
   phys::df::computeBareDualGreensFunction(eps_cg, eps_tilde, Delta, G, concurrency_, G0_tilde);
+
+  for (int w = 0; w < MatsubaraFreqDmn::dmn_size(); ++w)
+    for (int k = 0; k < KSuperlatticeDmn::dmn_size(); ++k) {
+      EXPECT_DOUBLE_EQ(
+          -G_0 * 1. / (G_0 + 1. / ((Delta_0 + eps_cg_0) - eps_tilde(0, 0, k).real())) * G_0,
+          G0_tilde(0, 0, k, w).real());
+      EXPECT_DOUBLE_EQ(-G_0 * 1. / (G_1 + 1. / (-eps_tilde(0, 1, k).real())) * G_1,
+                       G0_tilde(0, 1, k, w).real());
+      EXPECT_DOUBLE_EQ(-G_1 * 1. / (G_0 + 1. / (-eps_tilde(1, 0, k).real())) * G_0,
+                       G0_tilde(1, 0, k, w).real());
+      EXPECT_DOUBLE_EQ(
+          -G_1 * 1. / (G_1 + 1. / ((Delta_1 + eps_cg_1) - eps_tilde(1, 1, k).real())) * G_1,
+          G0_tilde(1, 1, k, w).real());
+    }
+
+  // Imaginary part should be zero since all input functions are real.
+  for (int w = 0; w < MatsubaraFreqDmn::dmn_size(); ++w)
+    for (int K2 = 0; K2 < KClusterDmn::dmn_size(); ++K2)
+      for (int K1 = 0; K1 < KClusterDmn::dmn_size(); ++K1)
+        for (int k = 0; k < KSuperlatticeDmn::dmn_size(); ++k)
+          EXPECT_EQ(0., G0_tilde(K1, K2, k, w).imag());
 }
