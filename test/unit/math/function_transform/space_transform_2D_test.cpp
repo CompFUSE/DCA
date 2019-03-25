b4c7d1b187a2081897e9c3d1642c19d7f5e24b65
Urs Haehner <haehneru@itp.phys.ethz.ch>
2019-03-25 15:27:58 +0100

    Put common test setup in test fixture class.

--- test/unit/math/function_transform/space_transform_2D_test.cpp
+++ test/unit/math/function_transform/space_transform_2D_test.cpp
@@ -23,22 +23,28 @@
 
 using namespace dca;
 
-TEST(SpaceTransform2DTest, f_r_r_otherDmns) {
-  const int dimension = 2;
-
-  const std::array<double, 4> basis{1., 0., 0., 1.};
-  const std::vector<std::vector<int>> superbasis{{2, 2}, {2, -2}};
+class SpaceTransform2DTest : public ::testing::Test {
+protected:
+  static constexpr int dimension = 2;
 
   using RDmn = phys::ClusterDomainAliases<dimension>::RClusterDmn;
   using KDmn = phys::ClusterDomainAliases<dimension>::KClusterDmn;
-  phys::domains::cluster_domain_initializer<RDmn>::execute(basis.data(), superbasis);
 
   using OtherDmns =
       func::dmn_variadic<func::dmn_0<func::dmn<2, double>>, func::dmn_0<func::dmn<3, int>>>;
 
-  func::function<std::complex<double>, func::dmn_variadic<RDmn, RDmn, OtherDmns>> f_r_r;
-  func::function<std::complex<double>, func::dmn_variadic<KDmn, KDmn, OtherDmns>> f_k_k;
+  static void SetUpTestCase() {
+    const std::array<double, 4> basis{1., 0., 0., 1.};
+    const std::vector<std::vector<int>> superbasis{{2, 2}, {2, -2}};
 
+    phys::domains::cluster_domain_initializer<RDmn>::execute(basis.data(), superbasis);
+  }
+
+  func::function<std::complex<double>, func::dmn_variadic<RDmn, RDmn, OtherDmns>> f_r_r_;
+  func::function<std::complex<double>, func::dmn_variadic<KDmn, KDmn, OtherDmns>> f_k_k_;
+};
+
+TEST_F(SpaceTransform2DTest, RealSpaceToMomentumSpaceSingleBand) {
   // Simplest test function:
   //     f(\vec{r1}, \vec{r2}, j) = j + j*j i, if \vec{r1} = \vec{r2} = 0,
   //                              = 0, otherwise.
@@ -46,27 +52,27 @@ TEST(SpaceTransform2DTest, f_r_r_otherDmns) {
   //    \hat{f}(\vec{k1}, \vec{k2}, j) = j/Nc + j*j/Nc i,
   // where Nc is the number of vectors \vec{R}.
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
-    f_r_r(0, 0, j) = std::complex<double>(j, j * j);
+    f_r_r_(0, 0, j) = std::complex<double>(j, j * j);
   }
 
-  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r, f_k_k);
+  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r_, f_k_k_);
 
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
     for (int k2 = 0; k2 < KDmn::dmn_size(); ++k2) {
       for (int k1 = 0; k1 < KDmn::dmn_size(); ++k1) {
-        EXPECT_DOUBLE_EQ((1. * j) / RDmn::dmn_size(), f_k_k(k1, k2, j).real());
-        EXPECT_DOUBLE_EQ((1. * j * j) / RDmn::dmn_size(), f_k_k(k1, k2, j).imag());
+        EXPECT_DOUBLE_EQ((1. * j) / RDmn::dmn_size(), f_k_k_(k1, k2, j).real());
+        EXPECT_DOUBLE_EQ((1. * j * j) / RDmn::dmn_size(), f_k_k_(k1, k2, j).imag());
       }
     }
   }
 
   // Check plus sign in exponential of first argument.
-  f_r_r = 0.;
+  f_r_r_ = 0.;
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
-    f_r_r(1, 0, j) = std::complex<double>(j, j * j);
+    f_r_r_(1, 0, j) = std::complex<double>(j, j * j);
   }
 
-  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r, f_k_k);
+  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r_, f_k_k_);
 
   const std::complex<double> i(0., 1.);
   const auto r1 = RDmn::get_elements()[1];
@@ -74,23 +80,23 @@ TEST(SpaceTransform2DTest, f_r_r_otherDmns) {
 
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
     const auto expected =
-        1. / RDmn::dmn_size() * f_r_r(1, 0, j) * std ::exp(i * (k1[0] * r1[0] + k1[1] * r1[1]));
-    EXPECT_DOUBLE_EQ(expected.real(), f_k_k(1, 0, j).real());
-    EXPECT_DOUBLE_EQ(expected.imag(), f_k_k(1, 0, j).imag());
+        1. / RDmn::dmn_size() * f_r_r_(1, 0, j) * std ::exp(i * (k1[0] * r1[0] + k1[1] * r1[1]));
+    EXPECT_DOUBLE_EQ(expected.real(), f_k_k_(1, 0, j).real());
+    EXPECT_DOUBLE_EQ(expected.imag(), f_k_k_(1, 0, j).imag());
   }
 
   // Check minus sign in exponential of second argument.
-  f_r_r = 0.;
+  f_r_r_ = 0.;
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
-    f_r_r(0, 1, j) = std::complex<double>(j, j * j);
+    f_r_r_(0, 1, j) = std::complex<double>(j, j * j);
   }
 
-  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r, f_k_k);
+  math::transform::SpaceTransform2D<RDmn, KDmn>::execute(f_r_r_, f_k_k_);
 
   for (int j = 0; j < OtherDmns::dmn_size(); ++j) {
     const auto expected =
-        1. / RDmn::dmn_size() * f_r_r(0, 1, j) * std ::exp(-i * (k1[0] * r1[0] + k1[1] * r1[1]));
-    EXPECT_DOUBLE_EQ(expected.real(), f_k_k(0, 1, j).real());
-    EXPECT_DOUBLE_EQ(expected.imag(), f_k_k(0, 1, j).imag());
+        1. / RDmn::dmn_size() * f_r_r_(0, 1, j) * std ::exp(-i * (k1[0] * r1[0] + k1[1] * r1[1]));
+    EXPECT_DOUBLE_EQ(expected.real(), f_k_k_(0, 1, j).real());
+    EXPECT_DOUBLE_EQ(expected.imag(), f_k_k_(0, 1, j).imag());
   }
 }
