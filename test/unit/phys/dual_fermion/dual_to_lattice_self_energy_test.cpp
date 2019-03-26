a646627eba897f74da4990833980e548af9a9d9a
Urs Haehner <haehneru@itp.phys.ethz.ch>
2019-03-26 14:08:46 +0100

    Add test for ComputeNonDiagonalLatticeSelfEnergy.

--- test/unit/phys/dual_fermion/dual_to_lattice_self_energy_test.cpp
+++ test/unit/phys/dual_fermion/dual_to_lattice_self_energy_test.cpp
@@ -20,8 +20,11 @@
 
 #include "dca/function/domains.hpp"
 #include "dca/function/function.hpp"
+#include "dca/math/function_transform/function_transform.hpp"
+#include "dca/math/util/vector_operations.hpp"
 #include "dca/parallel/no_concurrency/no_concurrency.hpp"
 #include "dca/phys/domains/cluster/cluster_domain_initializer.hpp"
+#include "dca/phys/domains/cluster/cluster_operations.hpp"
 
 using namespace dca;
 
@@ -49,14 +52,21 @@ protected:
   using DualToLatticeSelfEnergyType =
       phys::df::DualToLatticeSelfEnergy<double, parallel::NoConcurrency, dimension_>;
 
+  using Complex = DualToLatticeSelfEnergyType::Complex;
+
   using BandDmn = DualToLatticeSelfEnergyType::BandDmn;
   using SpinDmn = DualToLatticeSelfEnergyType::SpinDmn;
 
   using SpFreqDmn = DualToLatticeSelfEnergyType::SpFreqDmn;
 
   using KClusterDmn = DualToLatticeSelfEnergyType::KClusterDmn;
+  using RClusterDmn = func::dmn_0<KClusterDmn::parameter_type::dual_type>;
+
   using KLatticeDmn = DualToLatticeSelfEnergyType::KLatticeDmn;
+  using RLatticeDmn = func::dmn_0<KLatticeDmn::parameter_type::dual_type>;
+
   using KSuperlatticeDmn = DualToLatticeSelfEnergyType::KSuperlatticeDmn;
+  using RSuperlatticeDmn = func::dmn_0<KSuperlatticeDmn::parameter_type::dual_type>;
 
   using SpClusterGF = DualToLatticeSelfEnergyType::SpClusterGF;
   using SpLatticeGF = DualToLatticeSelfEnergyType::SpLatticeGF;
@@ -64,8 +74,8 @@ protected:
 
   DualToLatticeSelfEnergyTest()
       : concurrency_(0, nullptr),
-        dual_to_lattice_comp_(concurrency_, G_cluster_, Sigma_cluster_, Sigma_dual_, Sigma_lattice_) {
-  }
+        dual_to_lattice_comp_(concurrency_, G_cluster_, Sigma_cluster_, Sigma_dual_,
+                              Sigma_lattice_) {}
 
   static void SetUpTestCase() {
     const int num_bands = 1;
@@ -76,15 +86,17 @@ protected:
 
     const std::array<double, 4> cluster_basis{1., 0., 0., 1.};
     const std::vector<std::vector<int>> cluster_superbasis{{2, 0}, {0, 1}};
-    phys::domains::cluster_domain_initializer<func::dmn_0<KClusterDmn::parameter_type::dual_type>>::execute(
-        cluster_basis.data(), cluster_superbasis);
+    phys::domains::cluster_domain_initializer<
+        func::dmn_0<KClusterDmn::parameter_type::dual_type>>::execute(cluster_basis.data(),
+                                                                      cluster_superbasis);
 
     const std::array<double, 4> superlattice_basis{2., 0., 0., 1.};
     const int superlattice_size = 2;
     const std::vector<std::vector<int>> superlattice_superbasis{{superlattice_size, 0},
                                                                 {0, superlattice_size}};
-    phys::domains::cluster_domain_initializer<func::dmn_0<KSuperlatticeDmn::parameter_type::dual_type>>::execute(
-        superlattice_basis.data(), superlattice_superbasis);
+    phys::domains::cluster_domain_initializer<
+        func::dmn_0<KSuperlatticeDmn::parameter_type::dual_type>>::execute(superlattice_basis.data(),
+                                                                           superlattice_superbasis);
 
     std::vector<std::vector<int>> lattice_superbasis(cluster_superbasis);
     for (int i = 0; i < dimension_; ++i) {
@@ -92,8 +104,9 @@ protected:
         lattice_superbasis[i][d] *= superlattice_size;
       }
     }
-    phys::domains::cluster_domain_initializer<func::dmn_0<KLatticeDmn::parameter_type::dual_type>>::execute(
-        cluster_basis.data(), lattice_superbasis);
+    phys::domains::cluster_domain_initializer<
+        func::dmn_0<KLatticeDmn::parameter_type::dual_type>>::execute(cluster_basis.data(),
+                                                                      lattice_superbasis);
   }
 
   static const testing::MockParameters parameters_;
@@ -112,10 +125,10 @@ const testing::MockParameters DualToLatticeSelfEnergyTest::parameters_;
 
 TEST_F(DualToLatticeSelfEnergyTest, ComputeNonDiagonalLatticeSelfEnergy) {
   // Prepare input.
-  std::complex<double> Sigma_dual_00(1., 1.);
-  std::complex<double> Sigma_dual_01(2., 4.);
-  std::complex<double> Sigma_dual_10(3., 9.);
-  std::complex<double> Sigma_dual_11(4., 16.);
+  Complex Sigma_dual_00(1., 1.);
+  Complex Sigma_dual_01(2., 4.);
+  Complex Sigma_dual_10(3., 9.);
+  Complex Sigma_dual_11(4., 16.);
   for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
     for (int k_tilde = 0; k_tilde < KSuperlatticeDmn::dmn_size(); ++k_tilde) {
       Sigma_dual_(0, 0, k_tilde, w) = Sigma_dual_00;
@@ -125,8 +138,8 @@ TEST_F(DualToLatticeSelfEnergyTest, ComputeNonDiagonalLatticeSelfEnergy) {
     }
   }
 
-  std::complex<double> Sigma_cluster_0(3.14, 1.59);
-  std::complex<double> Sigma_cluster_1(2.65, 3.59);
+  Complex Sigma_cluster_0(3.14, 1.59);
+  Complex Sigma_cluster_1(2.65, 3.59);
   for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
     for (int s = 0; s < SpinDmn::dmn_size(); ++s) {
       Sigma_cluster_(0, s, 0, s, 0, w) = Sigma_cluster_0;
@@ -134,8 +147,8 @@ TEST_F(DualToLatticeSelfEnergyTest, ComputeNonDiagonalLatticeSelfEnergy) {
     }
   }
 
-  std::complex<double> G_cluster_0(4.2, 2.4);
-  std::complex<double> G_cluster_1(3.7, 7.3);
+  Complex G_cluster_0(4.2, 2.4);
+  Complex G_cluster_1(3.7, 7.3);
   for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
     for (int s = 0; s < SpinDmn::dmn_size(); ++s) {
       G_cluster_(0, s, 0, s, 0, w) = G_cluster_0;
@@ -189,4 +202,86 @@ TEST_F(DualToLatticeSelfEnergyTest, ComputeNonDiagonalLatticeSelfEnergy) {
                   Sigma_lattice_nondiag_K(1, 1, k_tilde, w).imag(), tol);
     }
   }
+}
+
+TEST_F(DualToLatticeSelfEnergyTest, ComputeDiagonalLatticeSelfEnergy) {
+  // Prepare diagonal (in K) input.
+  DualGF Sigma_lattice_nondiag;
+  for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
+    for (int k_tilde = 0; k_tilde < KSuperlatticeDmn::dmn_size(); ++k_tilde) {
+      for (int K = 0; K < KClusterDmn::dmn_size(); ++K) {
+        const Complex value(K + std::sin(w), k_tilde - std::cos(w));
+        Sigma_lattice_nondiag(K, K, k_tilde, w) = value;
+      }
+    }
+  }
+  dual_to_lattice_comp_.setNonDiagonalLatticeSelfEnergy(Sigma_lattice_nondiag);
+
+  // Compute expected result.
+  // - Copy diagonal into single argument function.
+  func::function<Complex, func::dmn_variadic<KClusterDmn, KSuperlatticeDmn, SpFreqDmn>>
+      Sigma_lattice_K_k_tilde;
+  for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
+    for (int k_tilde = 0; k_tilde < KSuperlatticeDmn::dmn_size(); ++k_tilde) {
+      for (int K = 0; K < KClusterDmn::dmn_size(); ++K) {
+        Sigma_lattice_K_k_tilde(K, k_tilde, w) = Sigma_lattice_nondiag(K, K, k_tilde, w);
+      }
+    }
+  }
+
+  // - Separate momentum to real space FTs for cluster and superlattice arguments.
+  func::function<Complex, func::dmn_variadic<RClusterDmn, KSuperlatticeDmn, SpFreqDmn>>
+      Sigma_lattice_R_k_tilde;
+  math::transform::FunctionTransform<KClusterDmn, RClusterDmn>::execute(Sigma_lattice_K_k_tilde,
+                                                                        Sigma_lattice_R_k_tilde);
+  func::function<Complex, func::dmn_variadic<RClusterDmn, RSuperlatticeDmn, SpFreqDmn>>
+      Sigma_lattice_R_r_tilde;
+  math::transform::FunctionTransform<KSuperlatticeDmn, RSuperlatticeDmn>::execute(
+      Sigma_lattice_R_k_tilde, Sigma_lattice_R_r_tilde);
+
+  // - Combine cluster and superlattice arguments into single lattice argument and add band and spin
+  //   dependecy.
+  func::function<Complex,
+                 func::dmn_variadic<BandDmn, SpinDmn, BandDmn, SpinDmn, RLatticeDmn, SpFreqDmn>>
+      Sigma_lattice_r;
+  for (int w = 0; w < SpFreqDmn::dmn_size(); ++w) {
+    for (int r_tilde = 0; r_tilde < RSuperlatticeDmn::dmn_size(); ++r_tilde) {
+      const auto& r_tilde_vec = RSuperlatticeDmn::get_elements()[r_tilde];
+
+      for (int R = 0; R < RClusterDmn::dmn_size(); ++R) {
+        const auto& R_vec = RClusterDmn::get_elements()[R];
+
+        const auto R_plus_r_tilde = math::util::add(R_vec, r_tilde_vec);
+
+        const auto r = phys::domains::cluster_operations::index(
+            phys::domains::cluster_operations::find_closest_cluster_vector(
+                R_plus_r_tilde, RLatticeDmn::get_elements(),
+                RLatticeDmn::parameter_type::get_super_basis_vectors()),
+            RLatticeDmn::get_elements(), RLatticeDmn::parameter_type::SHAPE);
+
+        Sigma_lattice_r(0, 0, 0, 0, r, w) = Sigma_lattice_R_r_tilde(R, r_tilde, w);
+        Sigma_lattice_r(0, 1, 0, 1, r, w) = Sigma_lattice_R_r_tilde(R, r_tilde, w);
+      }
+    }
+  }
+
+  // - Lattice FT to momentum space.
+  SpLatticeGF Sigma_lattice_expected;
+  math::transform::FunctionTransform<RLatticeDmn, KLatticeDmn>::execute(Sigma_lattice_r,
+                                                                        Sigma_lattice_expected);
+
+  dual_to_lattice_comp_.computeDiagonalLatticeSelfEnergy();
+
+  // Check results.
+  for (int w = 0; w < SpFreqDmn::dmn_size(); ++w)
+    for (int k = 0; k < KLatticeDmn::dmn_size(); ++k)
+      for (int s2 = 0; s2 < SpinDmn::dmn_size(); ++s2)
+        for (int b2 = 0; b2 < BandDmn::dmn_size(); ++b2)
+          for (int s1 = 0; s1 < SpinDmn::dmn_size(); ++s1)
+            for (int b1 = 0; b1 < BandDmn::dmn_size(); ++b1) {
+              EXPECT_DOUBLE_EQ(Sigma_lattice_expected(b1, s1, b2, s2, k, w).real(),
+                               Sigma_lattice_(b1, s1, b2, s2, k, w).real());
+              EXPECT_DOUBLE_EQ(Sigma_lattice_expected(b1, s1, b2, s2, k, w).imag(),
+                               Sigma_lattice_(b1, s1, b2, s2, k, w).imag());
+            }
 }
