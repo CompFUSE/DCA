6a1a29b9762ab82e45eb19cbba980d87c1248134
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2019-09-09 18:09:34 -0400

    Function copy/move constructors copy the name.

--- include/dca/phys/dca_step/cluster_mapping/coarsegraining/coarsegraining_tp.hpp
+++ include/dca/phys/dca_step/cluster_mapping/coarsegraining/coarsegraining_tp.hpp
@@ -205,7 +205,8 @@ void coarsegraining_tp<parameters_type, K_dmn>::execute(
                                                  K_dmn::get_elements(), K_dmn::parameter_type::SHAPE);
   interpolation_matrices<scalar_type, k_HOST, q_plus_Q_dmn>::set_q_idx(Q_ind);
 
-  switch (parameters.get_four_point_type()) {
+  // TODO: allow more than one channel.
+  switch (parameters.get_channels()[0]) {
     case PARTICLE_HOLE_CHARGE:
     case PARTICLE_HOLE_MAGNETIC:
     case PARTICLE_HOLE_TRANSVERSE: {
@@ -233,7 +234,8 @@ void coarsegraining_tp<parameters_type, K_dmn>::execute(
   int Q_ind = domains::cluster_operations::index(parameters.get_four_point_momentum_transfer(),
                                                  K_dmn::get_elements(), K_dmn::parameter_type::SHAPE);
 
-  switch (parameters.get_four_point_type()) {
+  // TODO: allow more than one channel.
+  switch (parameters.get_channels()[0]) {
     case PARTICLE_HOLE_CHARGE:
     case PARTICLE_HOLE_MAGNETIC:
     case PARTICLE_HOLE_TRANSVERSE: {
@@ -620,7 +622,8 @@ void coarsegraining_tp<parameters_type, K_dmn>::find_w1_and_w2(std::vector<doubl
 
   assert(std::abs(w::get_elements()[w1] - elements[w_ind]) < 1.e-6);
 
-  switch (parameters.get_four_point_type()) {
+  // TODO: allow more than one channel.
+  switch (parameters.get_channels()[0]) {
     case PARTICLE_HOLE_CHARGE:
     case PARTICLE_HOLE_MAGNETIC:
     case PARTICLE_HOLE_TRANSVERSE: {
@@ -647,7 +650,8 @@ void coarsegraining_tp<parameters_type, K_dmn>::compute_bubble(
       for (int n2 = 0; n2 < b::dmn_size(); n2++) {
         for (int m1 = 0; m1 < b::dmn_size(); m1++) {
           for (int m2 = 0; m2 < b::dmn_size(); m2++) {
-            switch (parameters.get_four_point_type()) {
+            // TODO: allow more than one channel.
+            switch (parameters.get_channels()[0]) {
               case PARTICLE_HOLE_TRANSVERSE:
                 bubble(n1, n2, m1, m2, q_ind) +=
                     G_q(n1, e_UP, m2, e_UP, q_ind) * G_q_plus_Q(n2, e_UP, m1, e_UP, q_ind);
@@ -680,7 +684,8 @@ void coarsegraining_tp<parameters_type, K_dmn>::compute_bubble(
 
 template <typename parameters_type, typename K_dmn>
 double coarsegraining_tp<parameters_type, K_dmn>::get_integration_factor() {
-  switch (parameters.get_four_point_type()) {
+  // TODO: allow more than one channel.
+  switch (parameters.get_channels()[0]) {
     case PARTICLE_HOLE_TRANSVERSE:
       return -1.;
       break;
--- include/dca/phys/dca_step/symmetrization/diagrammatic_symmetries.hpp
+++ include/dca/phys/dca_step/symmetrization/diagrammatic_symmetries.hpp
@@ -253,7 +253,7 @@ void diagrammatic_symmetries<parameters_type>::execute(
     }
 
     // TODO: allow multiple channels.
-    switch (parameters.get_channel()[0]) {
+    switch (parameters.get_channels()[0]) {
       case PARTICLE_HOLE_TRANSVERSE:
         symmetrize_over_pi_rotations_ph(G);
         break;
