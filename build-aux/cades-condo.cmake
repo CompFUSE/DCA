3cfd895667cfb016ad889af5611c0514b78e755d
Peter Doak <doakpw@ornl.gov>
2019-10-07 16:57:11 -0400

    working GPU build with llvm_dev

--- build-aux/cades-condo.cmake
+++ build-aux/cades-condo.cmake
@@ -4,8 +4,9 @@
 
 add_compile_options(-fPIC -march=native -mtune=native)
 
-set(FFTW_INCLUDE_DIR $ENV{FFTW_DIR}/include CACHE PATH "FFTW include path")
-set(FFTW_LIBRARY $ENV{FFTW_DIR}/lib/libfftw3.a CACHE FILEPATH "FFTW libary")
+set(FFTW_DIR /software/user_tools/centos-7.2.1511/cades-cnms/spack/opt/spack/linux-centos7-x86_64/clang-7.9.0/fftw-3.3.8-3mx5kwmdcznqlizyos5uipzhpj6jou2w)
+set(FFTW_INCLUDE_DIR ${FFTW_DIR}/include CACHE PATH "FFTW include path")
+set(FFTW_LIBRARY "${FFTW_DIR}/lib/libfftw3.a;${FFTW_DIR}/lib/libfftw3f.a" CACHE FILEPATH "FFTW libary")
 
 set(TEST_RUNNER "mpirun" CACHE STRING "Command for executing (MPI) programs.")
 set(MPIEXEC_NUMPROC_FLAG "-np" CACHE STRING
@@ -14,7 +15,7 @@ set(MPIEXEC_PREFLAGS "" CACHE STRING
   "Flags to pass to TEST_RUNNER directly before the executable to run.")
 
 # Enable the GPU support.
-option(DCA_WITH_CUDA "Enable GPU support." OFF)
+option(DCA_WITH_CUDA "Enable GPU support." ON)
 option(DCA_WITH_MPI "Enable MPI support." ON)
 # Compile for Volta compute architecture.
 set(CUDA_GPU_ARCH "sm_60" CACHE STRING "Name of the *real* architecture to build for.")
@@ -27,3 +28,5 @@ set(CUDA_TOOLKIT_ROOT_DIR $ENV{CUDA_DIR})
 
 option(DCA_WITH_TESTS_FAST "Enable fast tests" ON)
 
+# Followed by for "llvm7.9.0" i.e. dev llvm with its version set back for nvcc
+#rm -rf *; CXX=$(which mpic++) CC=$(which mpicc) CXXFLAGS="--gcc-toolchain=/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5" LDFLAGS="-L/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5/lib64 -Wl,-rpath,/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5" LD_FLAGS="-L/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5/lib64 -Wl,-rpath,/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5 -fuse-ld=lld" cmake -C../build-aux/cades-condo.cmake -DCMAKE_EXE_LINKER_FLAGS="-L/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5/lib64 -Wl,-rpath,/software/dev_tools/swtree/cs400_centos7.5_pe2018/gcc/8.1.0/centos7.5_gnu4.8.5 -fuse-ld=lld" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja ..
