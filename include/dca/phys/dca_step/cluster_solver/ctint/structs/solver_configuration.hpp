8e178b3340154629d22da8b4c2dba026e3be2467
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2019-10-08 16:03:26 +0200

    Updated configuration and interaction vertices to handle multiple insertions/removals.

--- include/dca/phys/dca_step/cluster_solver/ctint/structs/solver_configuration.hpp
+++ include/dca/phys/dca_step/cluster_solver/ctint/structs/solver_configuration.hpp
@@ -9,15 +9,17 @@
 //
 // This class organizes the vertex configuration for ct-int.
 
-#ifndef DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_CT_INT_CONFIGURATION_HPP
-#define DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_CT_INT_CONFIGURATION_HPP
+#ifndef DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_SOLVER_CONFIGURATION_HPP
+#define DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_SOLVER_CONFIGURATION_HPP
 
 #include <array>
+#include <numeric>
 #include <vector>
 
 #include "dca/io/buffer.hpp"
 #include "dca/phys/dca_step/cluster_solver/ctint/structs/ct_int_matrix_configuration.hpp"
 #include "dca/phys/dca_step/cluster_solver/ctint/structs/interaction_vertices.hpp"
+#include "dca/phys/dca_step/cluster_solver/ctint/structs/utils.hpp"
 #include "dca/linalg/device_type.hpp"
 
 namespace dca {
@@ -30,21 +32,25 @@ class SolverConfiguration : public MatrixConfiguration {
 public:
   using BaseClass = MatrixConfiguration;
 
-  inline SolverConfiguration() {}
-  inline SolverConfiguration(const SolverConfiguration& other) = default;
-  inline SolverConfiguration(double beta, int n_bands, const InteractionVertices& H_int,
-                             double double_move_prob = 0);
+  SolverConfiguration() {}
+  SolverConfiguration(const SolverConfiguration& other) = default;
+  SolverConfiguration(SolverConfiguration&& other) = default;
+  SolverConfiguration(double beta, int n_bands, const InteractionVertices& H_int,
+                      double double_move_prob = 0);
 
-  inline SolverConfiguration& operator=(const SolverConfiguration& other);
-  inline SolverConfiguration& operator=(SolverConfiguration&& other);
+  SolverConfiguration& operator=(const SolverConfiguration& other) = default;
+  SolverConfiguration& operator=(SolverConfiguration&& other) = default;
 
   bool operator==(const SolverConfiguration& rhs) const;
 
   template <class RngType>
   void insertRandom(RngType& rng);
 
+  // TODO: pass output as argument.
   template <class RngType>
-  std::pair<short, short> randomRemovalCandidate(RngType& rng) const;
+  std::vector<int> randomRemovalCandidate(RngType& rng, double removal_rand);
+  template <class RngType>
+  std::vector<int> randomRemovalCandidate(RngType& rng);
 
   // Remove elements with id remove by copying elements from the end.
   // Postcondition: from and sector_from contains the indices that where moved into the place
@@ -63,8 +69,31 @@ public:
   inline void pop(int n = 1);
   inline void push_back(const Vertex& v);
 
-  inline int occupationNumber(int vertex_index) const;
-  inline int occupationNumber(const Vertex& vertex) const;
+  inline int nPartners(int vertex_index) const;
+
+  void prepareForSubmatrixUpdate() {
+    removable_.resize(vertices_.size());
+    std::iota(removable_.begin(), removable_.end(), 0);
+  }
+
+  void commitInsertion(int idx) {
+    assert(idx < size() && idx >= removable_.size());
+    removable_.push_back(idx);
+    if (double_insertion_prob_) {
+      const auto tag = vertices_[idx].tag;
+      auto& list = existing_[vertices_[idx].interaction_id];
+      list.push_back(tag);
+    }
+  }
+
+  void markForRemoval(int idx) {
+    removable_.erase(std::find(removable_.begin(), removable_.end(), idx));
+    if (double_insertion_prob_) {
+      const auto tag = vertices_[idx].tag;
+      auto& list = existing_[vertices_[idx].interaction_id];
+      list.erase(std::find(list.begin(), list.end(), tag));
+    }
+  }
 
   std::size_t size() const {
     return vertices_.size();
@@ -79,25 +108,10 @@ public:
     return vertices_.back();
   }
 
-  const InteractionElement& coordinates(int v_idx) const {
-    assert(v_idx >= 0 and v_idx < (int)size());
-    return (*H_int_)[vertices_[v_idx].interaction_id];
-  }
-
   int get_bands() const {
     return n_bands_;
   }
 
-  const auto& getExisting(const short id) const {
-    return existing_[id];
-  }
-
-  inline ushort getLeftNu(const int matrix_index) const;
-  inline ushort getRightNu(const int matrix_index) const;
-  inline ushort getLeftR(const int matrix_index) const;
-  inline ushort getRightR(const int matrix_index) const;
-  inline double getTau(const int matrix_index) const;
-  inline bool getAuxSpin(int matrix_index) const;
   auto getTag(int i) const {
     return vertices_[i].tag;
   }
@@ -115,13 +129,30 @@ public:
   // Return index corresponding to tag.
   int findTag(std::uint64_t tag) const;
 
+  auto possiblePartners() const {
+      return H_int_->possiblePartners();
+  }
+
   friend io::Buffer& operator<<(io::Buffer& buff, const SolverConfiguration& config);
   friend io::Buffer& operator>>(io::Buffer& buff, SolverConfiguration& config);
 
 private:
-  inline void addSectorSizes(int idx, std::array<int, 2>& sizes) const;
+  const InteractionElement& coordinates(int v_idx) const {
+    assert(v_idx >= 0 and v_idx < (int)size());
+    return (*H_int_)[vertices_[v_idx].interaction_id];
+  }
+  void addSectorSizes(int idx, std::array<int, 2>& sizes) const;
 
-protected:
+  template <class Rng>
+  bool doDoubleUpdate(Rng& rng) const {
+    if (double_insertion_prob_ == 0)
+      return false;
+    else if (double_insertion_prob_ == 1)
+      return true;
+    else
+      return rng() < double_insertion_prob_;
+  }
+
   // List of points entering into the first or second member of g0
   const double double_insertion_prob_ = 0;
 
@@ -129,6 +160,8 @@ protected:
 
   const InteractionVertices* H_int_ = nullptr;
   std::vector<std::vector<std::uint64_t>> existing_;
+  std::vector<std::vector<std::size_t>*> partners_lists_;
+  std::vector<int> removable_;
   ushort last_insertion_size_ = 1;
   const double max_tau_ = 0;
   const int n_bands_ = 0;
@@ -136,24 +169,9 @@ protected:
   std::uint64_t current_tag_ = 0;
 };
 
-SolverConfiguration& SolverConfiguration::operator=(const SolverConfiguration& other) {
-  BaseClass::operator=(other);
-  H_int_ = other.H_int_;
-  vertices_ = other.vertices_;
-  existing_ = other.existing_;
-  return *this;
-}
-
-SolverConfiguration& SolverConfiguration::operator=(SolverConfiguration&& other) {
-  H_int_ = other.H_int_;
-  BaseClass::operator=(other);
-  vertices_ = std::move(other.vertices_);
-  return *this;
-}
-
-SolverConfiguration::SolverConfiguration(const double beta, const int n_bands,
-                                         const InteractionVertices& H_int,
-                                         const double double_insertion)
+inline SolverConfiguration::SolverConfiguration(const double beta, const int n_bands,
+                                                const InteractionVertices& H_int,
+                                                const double double_insertion)
     : MatrixConfiguration(&H_int, n_bands),
       double_insertion_prob_(double_insertion),
 
@@ -164,19 +182,19 @@ SolverConfiguration::SolverConfiguration(const double beta, const int n_bands,
 
 template <class RngType>
 void SolverConfiguration::insertRandom(RngType& rng) {
-  const std::pair<short, short> indices = H_int_->getInsertionIndices(rng());
+  const std::pair<short, short> indices = H_int_->getInsertionIndices(rng, double_insertion_prob_);
   const double tau = rng() * max_tau_;
   const bool aux_spin = rng() > 0.5;
 
-  push_back(Vertex{aux_spin, ushort(indices.first), ++current_tag_, tau});
+  push_back(Vertex{aux_spin, ushort(indices.first), current_tag_++, tau});
 
   if (double_insertion_prob_) {
     // TODO: generalize to multiband n_bands > 2
-    if (indices.second != -1 and rng() < double_insertion_prob_) {
+    if (indices.second != -1 && double_insertion_prob_) {
       const double tau2 = rng() * max_tau_;
       const bool aux_spin2 = rng() > 0.5;
       // TODO: decide if tag is same or not.
-      push_back(Vertex{aux_spin2, ushort(indices.second), ++current_tag_, tau2});
+      push_back(Vertex{aux_spin2, ushort(indices.second), current_tag_++, tau2});
       last_insertion_size_ = 2;
     }
     else
@@ -186,51 +204,47 @@ void SolverConfiguration::insertRandom(RngType& rng) {
 }
 
 template <class RngType>
-std::pair<short, short> SolverConfiguration::randomRemovalCandidate(RngType& rng) const {
-  std::pair<short, short> candidates(rng() * size(), -1);
-  if (double_insertion_prob_) {
-    const int partner_id = (*H_int_)[vertices_[candidates.first].interaction_id].partner_id;
-    if (partner_id != -1 and rng() < double_insertion_prob_) {
-      const std::size_t n_partners = existing_[partner_id].size();
-      if (n_partners) {
-        auto tag = existing_[partner_id][int(rng() * n_partners)];
-        candidates.second = findTag(tag);
-      }
-    }
+std::vector<int> SolverConfiguration::randomRemovalCandidate(RngType& rng)  {
+  return randomRemovalCandidate(rng, rng());
+}
+
+template <class RngType>
+std::vector<int> SolverConfiguration::randomRemovalCandidate(RngType& rng, double removal_rand) {
+  std::vector<int> candidates;
+  if (removable_.size() == 0)
+    return candidates;
+
+  candidates.push_back(removable_[removal_rand * removable_.size()]);
+
+  if ((*H_int_)[vertices_[candidates[0]].interaction_id].partners_id.size() &&
+      doDoubleUpdate(rng)) {  // Double removal.
+    partners_lists_.clear();
+    for (const auto& partner_id : (*H_int_)[vertices_[candidates[0]].interaction_id].partners_id)
+      partners_lists_.push_back(&existing_[partner_id]);
+
+    const auto tag = details::getRandomElement(partners_lists_, rng());
+    candidates.push_back(findTag(tag));
+    assert(candidates[1] < int(size()) && candidates[1] >= 0);
   }
-  assert(candidates.first < size() and candidates.second < int(size()));
+
+  assert(candidates[0] < int(size()));
   return candidates;
 }
 
 void SolverConfiguration::push_back(const Vertex& v) {
   vertices_.push_back(v);
   BaseClass::addVertex(v);
-
-  if (double_insertion_prob_)
-    existing_[v.interaction_id].push_back(v.tag);
 }
 
 void SolverConfiguration::pop(const int n) {
   assert(n <= (int)size());
-  assert(n == 1 or (*H_int_)[vertices_[size() - 2].interaction_id].partner_id ==
-                       vertices_[size() - 1].interaction_id);
-  if (double_insertion_prob_) {
-    for (int i = 1; i <= n; ++i) {
-      const int type = vertices_[size() - i].interaction_id;
-      auto& list = existing_[type];
-      const auto iter = std::find(list.begin(), list.end(), vertices_[size() - i].tag);
-      list.erase(iter);
-    }
-  }
+
   const int first_idx = size() - n;
   std::array<int, 2> removal_size{0, 0};
   for (std::size_t i = first_idx; i < size(); ++i)
     addSectorSizes(i, removal_size);
   vertices_.erase(vertices_.begin() + first_idx, vertices_.end());
   BaseClass::pop(removal_size[0], removal_size[1]);
-
-  // ctint base walker leaves configuration temporary inconsistent by (poor) design.
-  // assert(checkConsistency());
 }
 
 std::array<int, 2> SolverConfiguration::sizeIncrease() const {
@@ -242,27 +256,29 @@ std::array<int, 2> SolverConfiguration::sizeIncrease() const {
   return result;
 }
 
-void SolverConfiguration::addSectorSizes(int idx, std::array<int, 2>& sizes) const {
+inline void SolverConfiguration::addSectorSizes(int idx, std::array<int, 2>& sizes) const {
   auto spin = [=](ushort nu) { return nu >= n_bands_; };
   const auto& nu = coordinates(idx).nu;
   ++sizes[spin(nu[0])];
   ++sizes[spin(nu[2])];
 }
 
-short SolverConfiguration::getSign(const int vertex_index) const {
-  return (*H_int_)[vertices_[vertex_index].interaction_id].w >= 0 ? 1 : -1;
+inline short SolverConfiguration::getSign(const int vertex_index) const {
+  return getStrength(vertex_index) > 0 ? 1 : -1;
 }
 
-double SolverConfiguration::getStrength(int vertex_index) const {
+inline double SolverConfiguration::getStrength(int vertex_index) const {
   assert(vertex_index >= 0 && vertex_index < (int)size());
   return (*H_int_)[vertices_[vertex_index].interaction_id].w;
 }
 
-int SolverConfiguration::occupationNumber(int vertex_index) const {
-  return existing_[vertices_[vertex_index].interaction_id].size();
-}
-int SolverConfiguration::occupationNumber(const Vertex& vertex) const {
-  return existing_[vertex.interaction_id].size();
+int SolverConfiguration::nPartners(int vertex_index) const {
+  assert(vertex_index < vertices_.size());
+  const auto& partners_id = (*H_int_)[vertices_[vertex_index].interaction_id].partners_id;
+  int n_partners = 0;
+  for (const auto partner_id : partners_id)
+    n_partners += existing_[partner_id].size();
+  return n_partners;
 }
 
 template <class Alloc>
@@ -301,16 +317,6 @@ inline void SolverConfiguration::moveAndShrink(std::array<std::vector<int, Alloc
       ++source_idx;
     if (source_idx < size())
       from.push_back(source_idx);
-  }
-
-  // Remove from list of partners.
-  if (double_insertion_prob_) {
-    for (int i = 0; i < to.size(); ++i) {
-      const int type = vertices_[to[i]].interaction_id;
-      auto& list = existing_[type];
-      const auto iter = std::find(list.begin(), list.end(), vertices_[to[i]].tag);
-      list.erase(iter);
-    }
   }
 
   // Move and shrink configuration.
@@ -359,6 +365,12 @@ inline bool SolverConfiguration::checkConsistency() const {
       const auto& list = existing_[v.interaction_id];
       if (std::find(list.begin(), list.end(), v.tag) == list.end())
         return false;
+      // Check total size.
+      int size_sum = 0;
+      for (const auto& list : existing_)
+        size_sum += list.size();
+      if (size_sum != vertices_.size())
+        return false;
     }
   }
   return true;
@@ -376,4 +388,4 @@ inline int SolverConfiguration::findTag(std::uint64_t tag) const {
 }  // namespace phys
 }  // namespace dca
 
-#endif  // DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_CT_INT_CONFIGURATION_HPP
+#endif  // DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_SOLVER_CONFIGURATION_HPP
