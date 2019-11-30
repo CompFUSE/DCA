5f0f9fe3cb5132c03f977b8c2b2e02d329121d87
John Biddiscombe <biddisco@cscs.ch>
2019-11-29 23:06:49 -0500

    Cleaning up pool and affinity test compatibility

--- include/dca/parallel/hpx/hpx.hpp
+++ include/dca/parallel/hpx/hpx.hpp
@@ -146,9 +146,7 @@ public:
   // The DCA unit testing expects the size set to be returned
   // so we ignore the true thread pool size and return what DCA expects
   std::size_t size() const {
-    std::cout << "HPX threadpool size" << std::endl;
-    return pool_size;
-    // return hpx::get_num_worker_threads();
+    return pool_size; // hpx::get_num_worker_threads();
   }
 
   // Returns a static instance.
