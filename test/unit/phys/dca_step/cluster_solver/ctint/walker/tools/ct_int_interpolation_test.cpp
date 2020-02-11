04073a43cb5fa6a2bd1f0f9e1b24231761c84c3e
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-02-11 17:16:03 +0100

    Fixed test duplication.

--- test/unit/phys/dca_step/cluster_solver/ctint/walker/tools/ct_int_interpolation_test.cpp
+++ test/unit/phys/dca_step/cluster_solver/ctint/walker/tools/ct_int_interpolation_test.cpp
@@ -1,13 +1,3 @@
-// Copyright (C) 2018 ETH Zurich
-// Copyright (C) 2018 UT-Battelle, LLC
-// All rights reserved.
-// See LICENSE.txt for terms of usage.
-// See CITATION.md for citation guidelines, if DCA++ is used for scientific publications.
-//
-// Author: Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
-//
-// This file tests interpolation of G0 on the CPU.
-
 #include "dca/phys/dca_step/cluster_solver/ctint/walker/tools/g0_interpolation.hpp"
 
 #include "gtest/gtest.h"
@@ -19,7 +9,14 @@
 using std::cout;
 using std::endl;
 
-TEST(G0Interpolation, G0Interpolation) {
+template <typename Real>
+class G0InterpolationTest : public ::testing::Test{};
+
+using FloatingPointTypes = ::testing::Types<float, double>;
+TYPED_TEST_CASE(G0InterpolationTest, FloatingPointTypes);
+
+
+TYPED_TEST(G0InterpolationTest, G0Interpolation) {
   using dca::phys::domains::time_domain;
   using dca::func::dmn_0;
   using dca::func::dmn_variadic;
@@ -29,7 +26,7 @@ TEST(G0Interpolation, G0Interpolation) {
 
   // Initialize the domains.
   time_domain::initialize(pars);
-  // dca::phys::solver::ctint::PositiveTimeDomain::initialize();
+  //dca::phys::solver::ctint::PositiveTimeDomain::initialize();
 
   using TestDomain = dmn_variadic<LabelDmn, dmn_0<time_domain>>;
   dca::func::function<double, TestDomain> f;
@@ -40,7 +37,7 @@ TEST(G0Interpolation, G0Interpolation) {
     f(0, i) = std::sin(t);
     f(1, i) = std::sin(2 * t);
   }
-  dca::phys::solver::ctint::G0Interpolation<dca::linalg::CPU> g0(f);
+  dca::phys::solver::ctint::G0Interpolation<dca::linalg::CPU, TypeParam> g0(f);
 
   for (double x : {0., 0.5, 3., M_PI - 1e-3}) {
     EXPECT_NEAR(std::sin(x), g0(x, 0), 1e-3);
--- test/unit/phys/dca_step/cluster_solver/ctint/walker/tools/ct_int_interpolation_test_gpu.cpp
+++ test/unit/phys/dca_step/cluster_solver/ctint/walker/tools/ct_int_interpolation_test_gpu.cpp
@@ -1,13 +1,3 @@
-// Copyright (C) 2018 ETH Zurich
-// Copyright (C) 2018 UT-Battelle, LLC
-// All rights reserved.
-// See LICENSE.txt for terms of usage.
-// See CITATION.md for citation guidelines, if DCA++ is used for scientific publications.
-//
-// Author: Giovanni Balduzzi (gbalduzz@itp.phys.ethz.ch)
-//
-// This file tests the interpolation of G0 on the GPU.
-
 #include "dca/phys/dca_step/cluster_solver/ctint/walker/tools/g0_interpolation_gpu.hpp"
 
 #include <iostream>
@@ -20,7 +10,15 @@
 using std::cout;
 using std::endl;
 
-TEST(G0Interpolation, G0Interpolation) {
+template <typename ScalarType>
+class G0InterpolationGpuTest : public ::testing::Test {};
+
+using TestTypes = ::testing::Types<float, double>;
+TYPED_TEST_CASE(G0InterpolationGpuTest, TestTypes);
+
+TYPED_TEST(G0InterpolationGpuTest, G0Interpolation) {
+  using Real = TypeParam;
+
   using dca::phys::domains::time_domain;
   using dca::func::dmn_0;
   using dca::func::dmn_variadic;
@@ -37,15 +35,15 @@ TEST(G0Interpolation, G0Interpolation) {
 
   // Write function values.
   for (int i = 0; i < time_domain::get_size(); i++) {
-    const double t = time_domain::get_elements()[i];
+    const Real t = time_domain::get_elements()[i];
     f(0, i) = std::exp(t);
     f(1, i) = std::sin(t);
   }
-  dca::phys::solver::ctint::G0Interpolation<dca::linalg::CPU> g0_cpu(f);
+  dca::phys::solver::ctint::G0Interpolation<dca::linalg::CPU, Real> g0_cpu(f);
 
-  dca::phys::solver::ctint::G0Interpolation<dca::linalg::GPU> g0_gpu(f);
+  dca::phys::solver::ctint::G0Interpolation<dca::linalg::GPU, Real> g0_gpu(f);
 
-  for (double x : {0., 0.5, 3., M_PI - 1e-3}) {
+  for (Real x : {0., 0.5, 3., M_PI - 1e-3}) {
     EXPECT_NEAR(g0_cpu(x, 0), g0_gpu(x, 0), 1e-12);
     EXPECT_NEAR(g0_cpu(x, 1), g0_gpu(x, 1), 1e-12);
   }
