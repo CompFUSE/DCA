da5a30afdb23207e5e3360cc0be5e28bb7b9cd6c
Weile Wei <lokwei9@gmail.com>
2020-02-20 12:09:54 -0600

    adding more cmake options for laptop settings

--- build-aux/laptop.cmake
+++ build-aux/laptop.cmake
@@ -2,8 +2,7 @@
 #
 # Usage: cmake -C /path/to/this/file /path/to/DCA/source -D<option>=<value> ...
 
-# Prevent CMake from searching for BLAS and LAPACK libraries.
-# Paths to IBM's ESSL (preferred) and NETLIB-LAPACK will be set manually.
+set(CMAKE_BUILD_TYPE Debug CACHE STRING "Debug Type")
 
 # Use jsrun for executing the tests.
 set(TEST_RUNNER "mpiexec" CACHE STRING "Command for executing (MPI) programs.")
@@ -20,9 +19,11 @@ set(LAPACK_LIBRARIES /home/weile/project/dev/install/lapck/liblapack.so;/home/we
 set(FFTW_INCLUDE_DIRS /home/weile/project/dev/install/fftw3/include CACHE PATH "Path to fftw3.h.")
 set(FFTW_LIBRARIES /home/weile/project/dev/install/fftw3/lib/libfftw3.so CACHE FILEPATH "The FFTW3(-compatible) library.")
 
-option(DCA_WITH_TESTS_FAST ON)
-option(DCA_WITH_TESTS_EXTENSIVE ON)
-option(DCA_WITH_THREADED_SOLVER ON)
+option(DCA_WITH_TESTS_FAST "enable fast tests" ON)
+option(DCA_WITH_TESTS_EXTENSIVE "Enable extensive tests" ON)
+option(DCA_WITH_THREADED_SOLVER "Enable threaded solver" ON)
 set(FFTW_ROOT /home/weile/project/dev/install/fftw3/ CACHE PATH "Path to fftw3.h.")
 set(DART_TESTING_TIMEOUT 120)
 
+set(DCA_WITH_HPX ON)
+set(HPX_DIR /home/weile/project/dev/install/hpx_Debug/lib/cmake/HPX CACHE PATH "PATH to HPX")
