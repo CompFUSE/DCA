67ec3f785cd773b625ebd1dc11e445c006fe392c
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-03-05 19:23:12 -0500

    fixed double removal with empty list

--- src/phys/dca_step/cluster_solver/ctint/structs/utils.cpp
+++ src/phys/dca_step/cluster_solver/ctint/structs/utils.cpp
@@ -7,12 +7,11 @@
 //
 // Author:Giovanni Balduzzi (gbalduzz@phys.ethz.ch)
 //
-// This class organizes the vertex configuration for ct-int.
+// Helper methods implementation fot the ct-int configuration.
 
-#ifndef DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_UTIL_HPP
-#define DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_UTIL_HPP
+#include "dca/phys/dca_step/cluster_solver/ctint/structs/utils.hpp"
 
-#include <stdexcept>
+#include <cassert>
 #include <vector>
 
 namespace dca {
@@ -23,12 +22,27 @@ namespace details {
 // dca::phys::solver::ctint::details::
 
 unsigned getRandomElement(const std::vector<const std::vector<std::size_t>*>& v_ptrs,
-                          const double rand) noexcept;
+                          const double rand) noexcept {
+  assert(rand >= 0 && rand <= 1);
+
+  unsigned size = 0;
+  for (int i = v_ptrs.size() - 1; i >= 0; --i)
+    size += v_ptrs[i]->size();
+
+  if (size == 0)
+    return -1;
+
+  // TODO: use binary search or other efficient scheme.
+  unsigned idx = size * rand;
+  for (auto v_ptr : v_ptrs) {
+    if (idx < v_ptr->size())
+      return (*v_ptr)[idx];
+    idx -= v_ptr->size();
+  }
+}
 
 }  // namespace details
 }  // namespace ctint
 }  // namespace solver
 }  // namespace phys
 }  // namespace dca
-
-#endif  // DCA_PHYS_DCA_STEP_CLUSTER_SOLVER_CTINT_STRUCTS_UTIL_HPP
