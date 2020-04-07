acc0ed99c71a74a949aa92ddabbb5814bec05509
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-04-07 17:44:46 +0200

    perf test cleanup

--- test/performance/util/containers/random_access_map_perftest.cpp
+++ test/performance/util/containers/random_access_map_perftest.cpp
@@ -43,11 +43,12 @@ void init() {
   std::random_shuffle(vals.begin(), vals.end());
 }
 
-static void BM_StdMap(benchmark::State& state) {
+template <template <class, class> class Map>
+void performSTLTest(benchmark::State& state) {
   init();
-  std::map<int, int> map;
+  Map<int, int> map;
 
-  for (int i = 0; i < n_init; ++i)
+  for (int i = 0; i < state.range(0); ++i)
     map.insert(std::make_pair(keys[i], vals[i]));
 
   for (auto _ : state) {
@@ -57,47 +58,29 @@ static void BM_StdMap(benchmark::State& state) {
       map.erase(keys[i]);
   }
 }
-BENCHMARK(BM_StdMap);
 
-//static void BM_StdUMap(benchmark::State& state) {
-//    init();
-//    std::unordered_map<int, int> map;
-//
-//    for (int i = 0; i < n_init; ++i)
-//        map.insert(std::make_pair(keys[i], vals[i]));
-//
-//    for (auto _ : state) {
-//        for (int i = n_init; i < n_init + n_test; ++i)
-//            map.insert(std::make_pair(keys[i], vals[i]));
-//        for (int i = n_init; i < n_init + n_test; ++i)
-//            map.erase(keys[i]);
-//    }
-//}
-//BENCHMARK(BM_StdUMap);
+static void BM_StdMap(benchmark::State& state) {
+  performSTLTest<std::map>(state);
+}
+BENCHMARK(BM_StdMap)->Arg(100)->Arg(1000)->Arg(10000);
+
+static void BM_StdUMap(benchmark::State& state) {
+  performSTLTest<std::unordered_map>(state);
+}
+BENCHMARK(BM_StdUMap)->Arg(100)->Arg(1000)->Arg(10000);
 
 #ifdef HAVE_ABSL
 static void BM_AbslMap(benchmark::State& state) {
-    init();
-    absl::btree_map<int, int> map;
-
-    for (int i = 0; i < n_init; ++i)
-        map.insert(std::make_pair(keys[i], vals[i]));
-
-    for (auto _ : state) {
-        for (int i = n_init; i < n_init + n_test; ++i)
-            map.insert(std::make_pair(keys[i], vals[i]));
-        for (int i = n_init; i < n_init + n_test; ++i)
-            map.erase(keys[i]);
-    }
+  performSTLTest<absl::btree_map>(state);
 }
-BENCHMARK(BM_AbslMap);
+BENCHMARK(BM_AbslMap)->Arg(100)->Arg(1000)->Arg(10000);
 #endif
 
 static void BM_MyMap(benchmark::State& state) {
   init();
   dca::util::RandomAccessMap<int, int> map;
 
-  for (int i = 0; i < n_init; ++i)
+  for (int i = 0; i < state.range(0); ++i)
     map.insert(keys[i], vals[i]);
 
   for (auto _ : state) {
@@ -107,4 +90,4 @@ static void BM_MyMap(benchmark::State& state) {
       map.erase(keys[i]);
   }
 }
-BENCHMARK(BM_MyMap);
+BENCHMARK(BM_MyMap)->Arg(100)->Arg(1000)->Arg(10000);
