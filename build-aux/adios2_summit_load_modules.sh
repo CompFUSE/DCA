6750c8d1aaf0e818072737feb762840cdc60c569
Peter Doak <doakpw@ornl.gov>
2020-05-04 13:55:33 -0400

    temporarily (I hope) pointing to my build of adios2

---
+++ build-aux/adios2_summit_load_modules.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Loads all modules that are required to build DCA++ on ORNL's Summit supercomputer.
+# A reset is done at the beginning to restore to the default programming environment on Summit.
+#
+# Usage: source summit_load_modules.sh
+
+module reset
+module load gcc/8.1.1
+module load cuda
+module load magma/2.5.1
+module load hdf5
+module load fftw
+module load cmake
+module load netlib-lapack
+module load essl
+
+export CC=mpicc
+export CXX=mpicxx
+module load bzip2/1.0.6
