13147b53b340b893a456ceedb386f3b9f48c4dea
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2020-08-03 12:19:17 +0200

    Revert "Merge pull request #209 from PDoakORNL/prevent_multiGPU_tests"
    This reverts commit f3ac453e7c29b67f700ef7b7bbb468e109d9af48, reversing
    changes made to 0460fba9b80811781897e3311e582556cd754eff.

--- cmake/dca_testing.cmake
+++ cmake/dca_testing.cmake
@@ -24,7 +24,7 @@ include(CMakeParseArguments)
 # MPI or CUDA may be given to indicate that the test requires these libraries. MPI_NUMPROC is the
 # number of MPI processes to use for a test with MPI, the default value is 1.
 function(dca_add_gtest name)
-  set(options FAST EXTENSIVE STOCHASTIC PERFORMANCE GTEST_MAIN MPI CUDA CUDA_CVD)
+  set(options FAST EXTENSIVE STOCHASTIC PERFORMANCE GTEST_MAIN MPI CUDA)
   set(oneValueArgs MPI_NUMPROC)
   set(multiValueArgs INCLUDE_DIRS SOURCES LIBS)
   cmake_parse_arguments(DCA_ADD_GTEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -82,14 +82,6 @@ function(dca_add_gtest name)
     return()
   endif()
 
-  if (DCA_ADD_GTEST_CUDA_CVD AND NOT DCA_HAVE_CUDA )
-    return()
-  endif()
-
-  if (DCA_ADD_GTEST_CUDA_CVD AND (DCA_TEST_GPU_COUNT LESS 3) )
-    return()
-  endif()
-
   add_executable(${name} ${name}.cpp ${DCA_ADD_GTEST_SOURCES})
 
   # Create a macro with the project source dir. We use this as the root path for reading files in
@@ -104,7 +96,7 @@ function(dca_add_gtest name)
     target_link_libraries(${name} gtest ${DCA_ADD_GTEST_LIBS})
   endif()
 
-  if (DCA_ADD_GTEST_CUDA OR DCA_ADD_GTEST_CUDA_CVD)
+  if (DCA_ADD_GTEST_CUDA)
     target_include_directories(${name} PRIVATE ${CUDA_TOOLKIT_INCLUDE})
     target_link_libraries(${name} ${DCA_CUDA_LIBS})
     target_compile_definitions(${name} PRIVATE DCA_HAVE_CUDA)
@@ -113,11 +105,6 @@ function(dca_add_gtest name)
       target_compile_definitions(${name} PRIVATE DCA_HAVE_MAGMA)
     endif()
     cuda_add_cublas_to_target(${name})
-    # a less hacky way to do this would be good but this is used to test
-    # development only feature distributed G4 at the moment.
-    if (DCA_ADD_GTEST_CUDA_CVD)
-      set(CVD_LAUNCHER "${PROJECT_SOURCE_DIR}/test/cvdlauncher.sh")
-    endif()
   endif()
 
   target_include_directories(${name} PRIVATE
@@ -131,13 +118,13 @@ function(dca_add_gtest name)
 
     add_test(NAME ${name}
              COMMAND ${TEST_RUNNER} ${MPIEXEC_NUMPROC_FLAG} ${DCA_ADD_GTEST_MPI_NUMPROC}
-                     ${MPIEXEC_PREFLAGS} ${SMPIARGS_FLAG_MPI_CVD} ${CVD_LAUNCHER} "$<TARGET_FILE:${name}>")
+                     ${MPIEXEC_PREFLAGS} "$<TARGET_FILE:${name}>")
                  target_link_libraries(${name} ${MPI_C_LIBRARIES})
   else()
     if (TEST_RUNNER)
       add_test(NAME ${name}
                COMMAND ${TEST_RUNNER} ${MPIEXEC_NUMPROC_FLAG} 1
-	               ${MPIEXEC_PREFLAGS} ${SMPIARGS_FLAG_NOMPI} "$<TARGET_FILE:${name}>")
+	               ${MPIEXEC_PREFLAGS} "$<TARGET_FILE:${name}>")
     else (TEST_RUNNER)
       add_test(NAME ${name}
                COMMAND "$<TARGET_FILE:${name}>")
