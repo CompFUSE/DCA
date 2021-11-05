7df2acdcdb65558cb1fc66ea8c87c86cf1b146c0
David M. Rogers <predictivestatmech@gmail.com>
2021-11-05 11:37:59 -0400

    Added comments to build environment scripts.

--- build-aux/spock_cray_build.sh
+++ build-aux/spock_cray_build.sh
@@ -1,3 +1,4 @@
+
 cmake -DDCA_WITH_CUDA=OFF -DDCA_WITH_HIP=ON \
       -DFFTW_INCLUDE_DIR=${OLCF_FFTW_ROOT}/include \
       -DFFTW_LIBRARY=${OLCF_FFTW_ROOT}/lib/libfftw3.a \
@@ -5,4 +6,6 @@ cmake -DDCA_WITH_CUDA=OFF -DDCA_WITH_HIP=ON \
       -DROCM_ROOT=${ROCM_PATH} \
       -DDCA_WITH_TESTS_FAST=ON -DTEST_RUNNER="srun" \
       -DGPU_TARGETS=gfx906 \
+      -DCMAKE_C_COMPILER=cc \
+      -DCMAKE_CXX_COMPILER=CC \
       ..
