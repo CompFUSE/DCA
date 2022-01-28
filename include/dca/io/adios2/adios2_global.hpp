b89db08820e41afc1611dc07014914633f7c7c77
Peter Doak <doakpw@ornl.gov>
2022-01-28 11:30:09 -0500

    missing file

---
+++ include/dca/io/adios2/adios2_global.hpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2021 ETH Zurich
+// Copyright (C) 2021 UT-Battelle, LLC
+// All rights reserved.
+//
+// See LICENSE for terms of usage.
+// See CITATION.md for citation guidelines, if DCA++ is used for scientific publications.
+//
+// Author: Peter Doak (doakpw@ornl.gov)
+//
+// ADIOS2 global object
+
+
+/** For testing only, in main_dca adios2::ADIOS is a member of dca_loop
+ */
+class GlobalAdios
+{
+private:
+  GlobalAdios();
+public:
+  static adios2::ADIOS& getAdios()
+  {
+    static adios2::ADIOS adios_;
+    return adios_;
+  }
+};
