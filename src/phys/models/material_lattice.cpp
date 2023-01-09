d5fd1a6d49aa3d62dbb748367457a9e0f308c19e
Peter Doak <doakpw@ornl.gov>
2023-01-09 13:42:33 -0500

    missing explicit instantiation of NiO material classes

---
+++ src/phys/models/material_lattice.cpp
@@ -0,0 +1,10 @@
+#include "dca/phys/models/material_hamiltonians/material_lattice.hpp"
+
+namespace dca {
+namespace phys {
+namespace models {
+template class material_lattice<Material::NiO_symmetric, dca::phys::domains::no_symmetry<3>>;
+template class material_lattice<Material::NiO_unsymmetric, dca::phys::domains::no_symmetry<3>>;
+}  // namespace models
+}  // namespace phys
+}  // namespace dca
