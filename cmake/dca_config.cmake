4f17e1e504e646dbe10fc78ed3790961c0ea2e6e
Thomas Maier <maierta@ornl.gov>
2023-01-31 16:17:08 -0500

    small bug fixes

--- cmake/dca_config.cmake
+++ cmake/dca_config.cmake
@@ -121,9 +121,9 @@ endif()
 
 # Lattice type
 set(DCA_LATTICE "square" CACHE STRING "Lattice type, options are: bilayer | square | triangular |
-    Kagome | hund | twoband_Cu | threeband | FeAs | material_NiO | material_FeSn ")
+    Kagome | hund | twoband_Cu | threeband | FeAs | material_NiO | material_FeSn | material_CuO2_Emery")
 set_property(CACHE DCA_LATTICE PROPERTY STRINGS bilayer square triangular Kagome hund twoband_Cu threeband
-             FeAs material_NiO material_FeSn material_CuO2_Emergy)
+             FeAs material_NiO material_FeSn material_CuO2_Emery)
 
 if (DCA_LATTICE STREQUAL "bilayer")
   set(DCA_LATTICE_TYPE dca::phys::models::bilayer_lattice<PointGroup>)
@@ -174,12 +174,12 @@ elseif (DCA_LATTICE STREQUAL "material_FeSn")
       "dca/phys/models/material_hamiltonians/material_lattice.hpp")
   set(DCA_MODEL_IS_MATERIAL_LATTICE ON CACHE BOOL "is the model a material lattice")
 elseif (DCA_LATTICE STREQUAL "material_CuO2_Emery")
-  set(DCA_LATTICE_TYPE "dca::phys::models::material_lattice<dca::phys::models::Material::Emery, dca::phys::domains::${DCA_POINT_GROUP}>")
+  set(DCA_LATTICE_TYPE "dca::phys::models::material_lattice<dca::phys::models::Material::CuO2_Emery, dca::phys::domains::${DCA_POINT_GROUP}>")
   set(DCA_LATTICE_INCLUDE
       "dca/phys/models/material_hamiltonians/material_lattice.hpp")
   set(DCA_MODEL_IS_MATERIAL_LATTICE ON CACHE BOOL "is the model a material lattice")
 else()
-  message(FATAL_ERROR "Please set DCA_LATTICE to a valid option: bilayer | square | triangular | Kagome | hund | twoband_Cu | threeband | FeAs | material_NiO | material_FeSn. | material_CuO2_Emergy")
+  message(FATAL_ERROR "Please set DCA_LATTICE to a valid option: bilayer | square | triangular | Kagome | hund | twoband_Cu | threeband | FeAs | material_NiO | material_FeSn. | material_CuO2_Emery")
 endif()
 
 # Model type
