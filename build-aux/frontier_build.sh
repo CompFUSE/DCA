7f5c84d54e86b5d8b723609e20575aa3b4bfb09e
David M. Rogers <predictivestatmech@gmail.com>
2023-08-02 15:05:16 -0400

    Working build with DCA_PROFILER=PAPI

--- build-aux/frontier_build.sh
+++ build-aux/frontier_build.sh
@@ -1,10 +1,13 @@
 
+export CXXFLAGS="-I/opt/cray/pe/papi/6.0.0.17/include"
+export LDFLAGS="-L/opt/cray/pe/papi/6.0.0.17/lib -lpapi"
+
 cmake -DDCA_WITH_CUDA=off -DDCA_WITH_HIP=ON \
 	  -DFFTW_INCLUDE_DIR=${OLCF_FFTW_ROOT}/include \
 	  -DFFTW_LIBRARY=${OLCF_FFTW_ROOT}/lib/libfftw3.a \
 	  -DDCA_FIX_BROKEN_MPICH=ON \
 	  -DROCM_ROOT=${ROCM_PATH} \
-	  -GNinja \
+	  -DDCA_PROFILER=PAPI \
 	  -DDCA_WITH_TESTS_FAST=ON \
 	  -DTEST_RUNNER="srun" \
 	  -DGPU_TARGETS=gfx90a \
@@ -13,4 +16,5 @@ cmake -DDCA_WITH_CUDA=off -DDCA_WITH_HIP=ON \
 	  -DCMAKE_CXX_COMPILER=mpic++ \
 	  -DCMAKE_INSTALL_PREFIX=$INST \
 	  -DCMAKE_PREFIX_PATH=$INST \
+ 	  -GNinja \
 	  ..
