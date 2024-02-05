199c6ea06db7fe5237db51e17f370a669cef6358
Peter Doak <doakpw@ornl.gov>
2024-02-05 11:13:23 -0500

    changes to allow partial builds for changing models etc

--- build-aux/frontier_load_modules.sh
+++ build-aux/frontier_load_modules.sh
@@ -6,16 +6,24 @@
 #
 # Usage: source frontier_load_modules.sh
 
-module reset
-module load gcc/12.2.0
-module load rocm/5.7.0
-module load ninja
 
-# After 2 weeks of digging through opaque linking and runtime errors,
-# I have concluded that cray-libsci causes such a mess
-# that it's much easier to compile your own openblas
-# and magma rather than fuss with it.  I did the latter in 1 day.
-module unload cray-libsci
+module reset
+module load gcc/11.2.0
+module load openblas
+module load hdf5
+module load amd-mixed/5.3.0
+module load fftw
+module load cmake
+module load ninja
+module use /sw/frontier/magma/lmod/Core/cce-15.0.0
+module load magma/2.7.2
+module load craype-accel-amd-gfx90a
 
 export CC=mpicc
 export CXX=mpicxx
+
+export OPENBLAS_ROOT=${OLCF_OPENBLAS_ROOT}
+export HDF5_ROOT=${OLCF_HDF5_ROOT}
+export MAGMA_ROOT=/sw/frontier/magma/opt/linux-sles15-zen3/cce-15.0.0/magma-2.7.2-x7o7sph6npwb73t2leetbgpbypqyhtz6
+export FFTW_PATH=${OLCF_FFTW_ROOT}
+export LD_PRELOAD=/opt/cray/pe/lib64/cce/libtcmalloc_minimal.so.1
