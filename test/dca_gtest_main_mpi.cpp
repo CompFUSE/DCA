8f2ac3bc19368926366272701a30c0ce183fec4b
Peter Doak <doakpw@ornl.gov>
2024-03-25 11:41:31 -0400

    a try at dealing with adios2 linking cause unit test issues

---
+++ test/dca_gtest_main_mpi.cpp
@@ -0,0 +1,31 @@
+
+
+#include "dca/testing/gtest_h_w_warning_blocking.h"
+#include "dca/parallel/mpi_concurrency/mpi_concurrency.hpp"
+
+#ifdef DCA_HAVE_HPX
+#include <hpx/hpx_start.hpp>
+
+int hpx_main(int argc, char* argv[])
+{
+  dca::parallel::MPIConcurrency concurrency(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  auto ret_gtest = RUN_ALL_TESTS();
+  hpx::finalize();
+  return ret_gtest;
+}
+
+int main(int argc, char** argv) {
+  std::cout << "doing HPX gtest main\n";
+  hpx::start(argc, argv);
+  // returns the value that hpx::main returns
+  return hpx::stop();
+}
+
+#else
+int main(int argc, char** argv) {
+  dca::parallel::MPIConcurrency concurrency(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+#endif
