0c3eceb72cff9ffc686d053d8961ebadd39b55aa
Peter Doak (epd) <doakpw@ornl.gov>
2025-01-22 13:44:19 -0500

    more fixes for ct_int gpu

--- src/phys/dca_step/cluster_solver/shared_tools/cluster_helper.cu
+++ src/phys/dca_step/cluster_solver/shared_tools/cluster_helper.cu
@@ -140,15 +140,22 @@ void ClusterHelper::set(int nc, const int* add, int lda, const int* sub, int lds
 #ifdef DEBUG_CLUSTER_HELPER
     checkClusterHelper<<<1, 1, 0, 0>>>(nc, lds);
 #endif
-    ||||||| parent of e155ca263 (through computeMInit looks good now for GPU) if (momentum) {
-      cudaMemcpyToSymbol(cluster_momentum_helper, &host_helper, sizeof(ClusterHelper));
     }
     else {
       // In debug on sdgx-2 for CTINT I see know evidence this actually works, it appears not to.
-      cudaMemcpyToSymbol(cluster_real_helper, &host_helper, sizeof(ClusterHelper),
-                         cudaMemcpyHostToDevice);
+      size_t cluster_helper_size;
+      checkRC(cudaGetSymbolSize(&cluster_helper_size, cluster_real_helper));
+      assert(cluster_helper_size  == sizeof(ClusterHelper));
+      checkRC(cudaMemcpyToSymbol(cluster_add_matrix, &host_helper.add_matrix_, sizeof(int*))
+				    );
+      checkRC(cudaMemcpyToSymbol(cluster_sub_matrix, &host_helper.sub_matrix_, sizeof(int*))
+				    );
+      checkRC(cudaMemcpyToSymbol(cluster_real_helper, &host_helper, cluster_helper_size));
       cudaDeviceSynchronize();
     }
+#ifndef NDEBUG
+    checkClusterHelper<<<1, 1, 0, 0>>>(nc, lds);
+#endif
   });
 }
 
