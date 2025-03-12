382436b2761cfa447d5f2eb55dcccebfd43d474d
Peter Doak <doakpw@ornl.gov>
2025-03-12 11:36:34 -0400

    more catches for exceptions in n_tools.

--- build-aux/frontier_load_modules.sh
+++ build-aux/frontier_load_modules.sh
@@ -8,15 +8,11 @@
 
 
 module reset
-module load gcc/11.2.0
-module load openblas
-module load hdf5
-module load amd-mixed/5.3.0
-module load fftw
+module load rocm/6.3.1
+module load openblas/0.3.26
+module load cray-hdf5
 module load cmake
 module load ninja
-module use /sw/frontier/magma/lmod/Core/cce-15.0.0
-module load magma/2.7.2
 module load craype-accel-amd-gfx90a
 
 export CC=mpicc
@@ -24,6 +20,7 @@ export CXX=mpicxx
 
 export OPENBLAS_ROOT=${OLCF_OPENBLAS_ROOT}
 export HDF5_ROOT=${OLCF_HDF5_ROOT}
-export MAGMA_ROOT=/sw/frontier/magma/opt/linux-sles15-zen3/cce-15.0.0/magma-2.7.2-x7o7sph6npwb73t2leetbgpbypqyhtz6
+export MAGMA_ROOT=/lustre/orion/world-shared/cph102/epd/opt/magma
 export FFTW_PATH=${OLCF_FFTW_ROOT}
 export LD_PRELOAD=/opt/cray/pe/lib64/cce/libtcmalloc_minimal.so.1
+export HDF5_ROOT=/lustre/orion/cph102/proj-shared/epd/spack/opt/spack/linux-sles15-zen3/rocmcc-6.0.0/hdf5-1.14.3-zb3ehzdumutzqowltqjhlzk6a3acywye
