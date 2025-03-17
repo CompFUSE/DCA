8042d9d4055ab36a3a6a3664ca2899683939e392
Peter Doak <doakpw@ornl.gov>
2025-03-17 09:34:54 -0400

    updated frontier build for ROCM631 with maximal modules

--- build-aux/frontier_load_modules.sh
+++ build-aux/frontier_load_modules.sh
@@ -15,6 +15,7 @@ module load cray-hdf5
 module load cmake
 module load ninja
 module load craype-accel-amd-gfx90a
+module load cray-fftw
 
 export CC=mpicc
 export CXX=mpicxx
@@ -24,4 +25,4 @@ export HDF5_ROOT=${OLCF_HDF5_ROOT}
 export MAGMA_ROOT=/lustre/orion/world-shared/cph102/epd/opt/magma
 export FFTW_PATH=${OLCF_FFTW_ROOT}
 #export LD_PRELOAD=/opt/cray/pe/lib64/cce/libtcmalloc_minimal.so.1
-export HDF5_ROOT=/lustre/orion/cph102/proj-shared/epd/spack/opt/spack/linux-sles15-zen3/rocmcc-6.0.0/hdf5-1.14.3-zb3ehzdumutzqowltqjhlzk6a3acywye
+#export HDF5_ROOT=/lustre/orion/cph102/proj-shared/epd/spack/opt/spack/linux-sles15-zen3/rocmcc-6.0.0/hdf5-1.14.3-zb3ehzdumutzqowltqjhlzk6a3acywye
